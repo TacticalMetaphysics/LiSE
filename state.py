186fb624887ca69af8f3395a2c09559d7f1cb6cc
LogicalDash <LogicalDash@gmail.com>
2013-05-12 15:30:00 -0400

    wrappers, and i don't know what happened to state.py, it just vanished or something

---
+++ state.py
@@ -0,0 +1,10 @@
+class GameState:
+    """Class to hold the state of the game, specifically not including the
+state of the interface.
+
+    """
+    def __init__(self, boarddict):
+        self.boarddict = boarddict
+
+    def update(self, ts, st):
+        pass
