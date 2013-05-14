0a94af41f183805b8a302b52feafb30900941697
LogicalDash <LogicalDash@gmail.com>
2013-05-14 06:20:35 -0400

    remove extant empty db before creating anew

--- mkdb.py
+++ mkdb.py
@@ -1,4 +1,5 @@
 import database
+import os
 from dimension import Dimension
 from item import Item, Thing, Place, Portal, Schedule, Journey
 from effect import Effect, EffectDeck
@@ -33,11 +34,19 @@ tabclasses = [
     Pawn,
     Board]
 
-db = database.Database('empty.sqlite')
+DB_NAME = 'empty.sqlite'
+
+try:
+    os.remove(DB_NAME)
+except IOError:
+    pass
+
+db = database.Database(DB_NAME)
 
 for clas in tabclasses:
     for tab in clas.schemata:
         print tab
         db.c.execute(tab)
 
-del db
+db.c.close()
+db.conn.commit()
