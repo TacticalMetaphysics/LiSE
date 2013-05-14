134b9769327022ee89311050d7f26c1fc4c6ce13
LogicalDash <LogicalDash@gmail.com>
2013-05-14 06:21:42 -0400

    importing db improvements from calendar branch

--- mkdb.py
+++ mkdb.py
@@ -1,6 +1,7 @@
 import database
+import os
 from dimension import Dimension
-from item import Item, Thing, Place, Portal, Journey, Schedule
+from item import Item, Thing, Place, Portal, Schedule, Journey
 from effect import Effect, EffectDeck
 from event import Event, EventDeck
 from style import Color, Style
@@ -33,11 +34,19 @@ tabclasses = [
     Pawn,
     Board]
 
-db = database.Database('empty.sqlite')
+DB_NAME = 'empty.sqlite'
+
+try:
+    os.remove(DB_NAME)
+except IOError:
+    pass
+
+db = database.Database(DB_NAME)
 
 for clas in tabclasses:
     for tab in clas.schemata:
         print tab
         db.c.execute(tab)
 
-del db
+db.c.close()
+db.conn.commit()
