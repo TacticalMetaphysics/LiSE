6a45dd72e4c8e65d5e0270d15bdcf8d34325eef5
LogicalDash <LogicalDash@gmail.com>
2013-05-20 16:35:03 -0400

    well, not much to document here for the moment really

--- lise.py
+++ lise.py
@@ -5,6 +5,14 @@ from state import GameState
 from board import load_board
 
 
+"""Run the map editor.
+
+On the command line, supply the database file name and the
+language.
+
+"""
+
+
 db = load_game("default.sqlite", "English")
 b = load_board(db, "Physical")
 s = GameState(db)
