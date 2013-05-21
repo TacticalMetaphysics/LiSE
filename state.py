afa2d90be90569a1a3edbfaa11244027726a715e
LogicalDash <LogicalDash@gmail.com>
2013-05-20 20:30:51 -0400

    there is nothing clever which can be said

--- state.py
+++ state.py
@@ -2,20 +2,28 @@ from event import get_all_starting_ongoing_ending as gasoe
 # do I want to handle the timer here? that might be good
 
 
+"""Controller for the whole thing, more or less."""
+
+
 class GameState:
     """Class to hold the state of the game, specifically not including the
 state of the interface.
 
     """
     def __init__(self, db):
+        """Return a GameState controlling everything in the given database."""
         self.db = db
         self.age = db.get_age()
         self.since = 0
 
     def __iter__(self):
+        """Return an iterator over the dimensions in the game."""
         return iter(self.dimensions)
 
     def update(self, ts, st):
+        """Update an appropriate number of ticks given that ts time has
+passed, and ticks are supposed to pass every st seconds. Both are
+floats."""
         # assuming for now that time only goes forward, at a rate of
         # one tick every st seconds
         self.since += ts
