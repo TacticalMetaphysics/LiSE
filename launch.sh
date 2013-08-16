2107c577dab2b927e47be0d52f7228d50da1816f
LogicalDash <LogicalDash@gmail.com>
2013-08-16 13:14:17 -0400

    Until I get to building a release tarball of my own I shall assume that anyone who checks out my sources knows how to compile igraph

--- launch.sh
+++
@@ -1,4 +0,0 @@
-# This file is part of LiSE, a framework for life simulation games.
-# Copyright (C) 2013 Zachary Spector,  zacharyspector@gmail.com
-dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-LD_LIBRARY_PATH="$dir/lib" PYTHONPATH="$dir/python-igraph*/" python lise.py
