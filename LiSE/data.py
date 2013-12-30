e49760d18e4230c29abcddc7dbbd4dd37b2a5406
Zachary Spector <zacharyspector@gmail.com>
2013-12-30 07:46:23 -0500

    Made a new way of loading charsheets and now NOTHING WORKS

--- LiSE/data.py
+++ LiSE/data.py
@@ -7,28 +7,20 @@
 def _(arg):
     return arg
 
-THING_LOC_TAB = 0
-THING_STAT_TAB = 1
-PLACE_STAT_TAB = 2
-PORTAL_LOC_TAB = 3
-PORTAL_STAT_TAB = 4
-CHAR_STAT_TAB = 5
-THING_LOC_CAL = 6
-THING_STAT_CAL = 7
-PLACE_STAT_CAL = 8
-PORTAL_ORIG_CAL = 9
-PORTAL_DEST_CAL = 10
-PORTAL_STAT_CAL = 11
-CHAR_STAT_CAL = 12
+THING_TAB = 0
+PLACE_TAB = 2
+PORTAL_TAB = 3
+CHAR_TAB = 4
+THING_LOC_CAL = 5
+THING_STAT_CAL = 6
+PLACE_STAT_CAL = 7
+PORTAL_ORIG_CAL = 8
+PORTAL_DEST_CAL = 9
+PORTAL_STAT_CAL = 10
+CHAR_STAT_CAL = 11
 
 
 SHEET_ITEM_TYPES = [
-    THING_LOC_TAB,
-    THING_STAT_TAB,
-    PLACE_STAT_TAB,
-    PORTAL_LOC_TAB,
-    PORTAL_STAT_TAB,
-    CHAR_STAT_TAB,
     THING_LOC_CAL,
     THING_STAT_CAL,
     PLACE_STAT_CAL,
@@ -39,12 +31,10 @@ SHEET_ITEM_TYPES = [
 
 
 TABLE_TYPES = [
-    THING_LOC_TAB,
-    THING_STAT_TAB,
-    PLACE_STAT_TAB,
-    PORTAL_LOC_TAB,
-    PORTAL_STAT_TAB,
-    CHAR_STAT_TAB]
+    THING_TAB,
+    PLACE_TAB,
+    PORTAL_TAB,
+    CHAR_TAB]
 
 
 CALENDAR_TYPES = [
@@ -56,22 +46,6 @@ CALENDAR_TYPES = [
     PORTAL_STAT_CAL,
     CHAR_STAT_CAL]
 
-
-ITEM_TYPE_TO_HEADERS = {
-    THING_LOC_TAB: [_("Thing"), _("Location")],
-    THING_STAT_TAB: [_("Thing")],
-    PLACE_STAT_TAB: [_("Place")],
-    PORTAL_LOC_TAB: [_("Portal"), _("Origin"), _("Destination")],
-    PORTAL_STAT_TAB: [_("Portal")],
-    CHAR_STAT_TAB: []}
-
-ITEM_TYPE_TO_FIELD_NAMES = {
-    THING_LOC_TAB: ["name", "location"],
-    THING_STAT_TAB: ["name"],
-    PLACE_STAT_TAB: ["name"],
-    PORTAL_LOC_TAB: ["name", "origin", "destination"],
-    PORTAL_STAT_TAB: ["name"],
-    CHAR_STAT_TAB: []}
 
 whole_imgrows = [
     ('default_wallpaper', 'LiSE/gui/assets/wallpape.jpg', 0, 0),
