4a9f5870d42f57cdb72a456cc5e5ee0d48d854b6
Zachary Spector <zacharyspector@gmail.com>
2014-02-01 18:59:51 -0500

    fix a bit of portaling

--- LiSE/gui/charsheet/item.py
+++ LiSE/gui/charsheet/item.py
@@ -3,11 +3,54 @@ from kivy.properties import (
     AliasProperty,
     DictProperty,
     ListProperty,
+    NumericProperty,
     ObjectProperty,
     ReferenceListProperty
 )
 from kivy.clock import Clock
 from kivy.logger import Logger
+
+from LiSE.gui.kivybits import ClosetButton
+
+
+class Sizer(ClosetButton):
+    csitem = ObjectProperty()
+    charsheet = AliasProperty(
+        lambda self: self.csitem.charsheet,
+        lambda self, v: None,
+        bind=('csitem',))
+    i = NumericProperty()
+
+    def __init__(self, **kwargs):
+        kwargs['size_hint_y'] = None
+        kwargs['height'] = 30
+        super(Sizer, self).__init__(**kwargs)
+
+    def on_touch_down(self, touch):
+        if self.collide_point(*touch.pos):
+            touch.ud['charsheet'] = self.charsheet
+            touch.ud['sizer'] = self
+            touch.grab(self)
+            self.prior_y = self.y
+            self.state = 'down'
+            touch.ud['sizer_i'] = self.i
+            return True
+
+    def on_touch_move(self, touch):
+        if 'sizer' not in touch.ud or touch.ud['sizer'] is not self:
+            touch.ungrab(self)
+            self.state = 'normal'
+            return
+        self.center_y = touch.pos
+        return True
+
+    def on_touch_up(self, touch):
+        if 'sizer' not in touch.ud or touch.ud['sizer'] is not self:
+            touch.ungrab(self)
+            self.state = 'normal'
+            return
+        self.state = 'normal'
+        return True
 
 
 class CharSheetItemButtonBox(BoxLayout):
@@ -17,7 +60,9 @@ class CharSheetItemButtonBox(BoxLayout):
 class CharSheetItem(BoxLayout):
     csbone = ObjectProperty()
     content = ObjectProperty()
+    sizer = ObjectProperty()
     buttons = ListProperty()
+    middle = ObjectProperty()
     item_class = ObjectProperty()
     item_kwargs = DictProperty()
     widspec = ReferenceListProperty(item_class, item_kwargs)
@@ -37,51 +82,47 @@ class CharSheetItem(BoxLayout):
         else None,
         lambda self, v: None,
         bind=('item_kwargs',))
-
-    def __init__(self, **kwargs):
-        self._trigger_set_bone = Clock.create_trigger(self._set_my_bone)
-        super(CharSheetItem, self).__init__(**kwargs)
-        self.finalize()
-
-    def on_csbone(self, *args):
-        self.height = self.csbone.height
-        self._trigger_set_bone()
-
-    def _set_my_bone(self, *args):
-        if not self.mybone:
-            return
-        self.closet.set_bone(self.mybone)
-
-    def _set_i(self, i):
-        if not self.csbone:
-            Logger.debug("{0}.i set before {0}.csbone; why?".format(self))
-            Clock.schedule_once(lambda dt: self.set_i(i), 0)
-            return
-        self.csbone = self.csbone._replace(idx=self.i)
-        self._trigger_set_bone()
-
     i = AliasProperty(
         lambda self: self.csbone.idx if self.csbone else -1,
-        _set_i,
+        lambda self, v: None,
         bind=('csbone',))
+
+    def __init__(self, **kwargs):
+        self._trigger_set_bone = Clock.create_trigger(
+            lambda dt: self.closet.set_bone(self.csbone))
+        kwargs['orientation'] = 'vertical'
+        super(CharSheetItem, self).__init__(**kwargs)
+        self.finalize()
 
     def upd_height(self, *args):
         if not self.csbone:
             Logger.debug("{0}.height set before {0}.csbone; why?".format(self))
-            Clock.schedule_once(self.on_height, 0)
+            Clock.schedule_once(self.upd_height, 0)
             return
-        self.csbone = self.csbone._replace(height=self.height)
-        self.content.height = self.height
-        self.buttons.height = self.height
-        self._trigger_set_bone()
+        elif self.i - 1 not in self.charsheet.i2wid:
+            Logger.debug("{0} seems to be the 0th charsheet item, but {0}.i=1".format(self))
+            Clock.schedule_once(self.upd_height, 0)
+            return
+        if self.sizer:
+            self.height = self.sizer.top - self.y
+            dh = self.height - self.csbone.height
+            wid_before = self.charsheet.i2wid[self.i-1]
+            wid_before.y += dh
+            wid_before.height -= dh
+        if self.height != self.csbone.height:
+            self.csbone = self.csbone._replace(height=self.height)
+            self.content.height = self.height
+            self.buttons.height = self.height
+            self._trigger_set_bone()
 
     def on_touch_move(self, touch):
         if not ('sizer_i' in touch.ud and touch.ud['sizer_i'] == self.i):
             return
         if 'wid_before' in touch.ud:
             return
-        touch.ud['wid_before'] = self
-        touch.ud['wid_after'] = self.charsheet.csitems[self.i+1]
+        # crappy hack to get the widget seen before me in the charsheet
+        touch.ud['wid_before'] = self.charsheet.i2wid[self.i-1]
+        touch.ud['wid_after'] = self
 
     def on_touch_up(self, touch):
         if 'wid_before' not in touch.ud:
@@ -97,12 +138,20 @@ class CharSheetItem(BoxLayout):
         if not self.item_class and self.item_kwargs:
             Clock.schedule_once(self.finalize, 0)
             return
+        self.middle = BoxLayout()
         self.content = self.item_class(**self.item_kwargs)
-        self.add_widget(self.content)
         buttonbox = CharSheetItemButtonBox(
             csitem=self,
             size_hint_x=0.2)
-        self.add_widget(buttonbox)
         self.buttons = buttonbox.children
-        buttonbox.bind(children=self.setter('buttons'))
+        self.middle.add_widget(self.content)
+        self.middle.add_widget(buttonbox)
+        if self.csbone.idx > 0:
+            self.sizer = Sizer(
+                csitem=self,
+                i=self.csbone.idx)
+            self.add_widget(self.sizer)
+            self.sizer.bind(top=self.upd_height)
+        self.add_widget(self.middle)
         self.bind(height=self.upd_height)
+        self.charsheet.i2wid[self.i] = self
