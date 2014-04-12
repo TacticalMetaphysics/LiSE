0d420976bf1915f83968fb1d697b53a30c80538e
Zachary Spector <zacharyspector@gmail.com>
2014-04-12 18:48:33 -0400

    Worked out a structure for new style charsheet

--- LiSE/gui/charsheet.py
+++ LiSE/gui/charsheet.py
@@ -10,19 +10,23 @@ from kivy.uix.stacklayout import StackLayout
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.widget import Widget
 from kivy.properties import (
+    BooleanProperty,
     ObjectProperty,
     StringProperty,
     OptionProperty,
     BoundedNumericProperty,
     ReferenceListProperty,
     ListProperty,
-    NumericProperty)
+    NumericProperty,
+    DictProperty)
 from kivy.graphics import (
     Color,
     Line,
     Triangle)
 
-from LiSE.gui.kivybits import ClosetLabel
+from LiSE.gui.kivybits import (
+    ClosetLabel,
+    SaveableWidgetMetaclass)
 
 
 class TimelineCell(ClosetLabel):
@@ -110,105 +114,47 @@ class TimelineCursor(Widget):
 
 class Timeline(StackLayout):
     """Blocks of time proceeding either left to right or top to bottom."""
-    charsheet = ObjectProperty()
-    """The CharSheet I am in"""
-    keys = ListProperty()
-    """Keys into the skeleton. These take me almost all the way down to
-    the lowest level of the skeleton; they lack only the present
-    branch.
-
-    """
-    data = ObjectProperty()
-    """Technically this contains a :class:`Skeleton`, but only the lowest
-    level of it, where it's a wrapper around an :class:`Array`.
-
-    """
-    field = StringProperty('value')
-    """What property of the bones of the variable represented here
-    contains the variable's present value?
-
-    Defaults to "value". This should become an OptionProperty once
-    I've settled on what field names to use for special data like
-    location.
-
-    """
-    branch = BoundedNumericProperty(None, min=0, allownone=True)
-    """If set, the timeline will only display data in this
-    branch. Otherwise it will display data from the present branch.
-
-    """
-    min_tick = BoundedNumericProperty(0, min=0)
-    """The first tick to show."""
-    max_tick = BoundedNumericProperty(100, min=0)
-    """The last tick to show."""
+    charsheet_item = ObjectProperty()
+    branch = NumericProperty(None, allownone=True)
+    data_iter = ObjectProperty()
+    ticks_wide = BoundedNumericProperty(75, min=2)
+    cursor_tick = NumericProperty()
 
     def __init__(self, **kwargs):
-        """Set up a trigger for self.redata, then call self.finalize()"""
+        self._trigger_refresh = Clock.create_trigger(self.refresh)
         super(Timeline, self).__init__(**kwargs)
-        self._trigger_redata = Clock.create_trigger(self.redata)
         self.finalize()
 
     def finalize(self, *args):
-        """Arrange to refresh my data whenever it changes, and whenever the
-        branch changes.
-
-        """
-        if not self.charsheet and self.keys:
+        if not self.charsheet_item and self.data_getter:
             Clock.schedule_once(self.finalize, 0)
             return
-        ptr = self.charsheet.closet.skeleton
-        for key in self.keys:
-            ptr = ptr[key]
-        ptr.register_listener(lambda child, k, v: self._trigger_redata)
-        self.charsheet.closet.timestream.register_branch_listener(
-            self._trigger_redata)
-        self.bind(branch=self._trigger_redata)
-        self.cursor = TimelineCursor(
-            timeline=self,
-            color=self.cursor_color)
-        self._trigger_redata()
+        self.refresh()
 
-    def redata(self, *args):
-        """Replace my data with the latest from the closet."""
-        ptr = self.charsheet.closet.skeleton
-        for key in self.keys:
-            ptr = ptr[key]
-        self.data = ptr[self.branch if self.branch
-                        else self.charsheet.character.closet.branch]
-
-    def iter_cells(self):
-        """Iterate over the bones in self.data, and yield TimelineCell
-        instances representing them."""
-        boneiter = self.data.iterbones()
-        bone = next(boneiter)
-        prev_tick = bone.tick
-        prev_value = getattr(bone, self.field)
-        while prev_tick < self.min_tick:
-            bone = next(boneiter)
-            prev_tick = bone.tick
-            prev_value = getattr(bone, self.field)
-        while bone.tick < self.max_tick:
-            bone = next(boneiter)
-            yield TimelineCell(
-                timeline=self,
-                stringname=prev_value,
-                tick_from=prev_tick,
-                tick_to=bone.tick)
-            prev_tick = bone.tick
-            prev_value = getattr(bone, self.field)
-
-    def on_data(self, *args):
-        """Clear old cells and add new ones."""
+    def refresh(self, *args):
         self.clear_widgets()
         for cell in self.iter_cells():
             self.add_widget(cell)
 
+    def iter_cells(self):
+        closet = self.charsheet_item.charsheet.observed.closet
+        branch = self.branch if self.branch else closet.branch
+        earliest = self.ticks_wide / 2
+        remainder = self.ticks_wide % 2
+        latest = self.ticks_wide + remainder
+        for (tick_from, tick_to, text) in self.data_iter(
+                branch, earliest, latest):
+            yield TimelineCell(
+                timeline=self,
+                stringname=text,
+                tick_from=tick_from,
+                tick_to=tick_to)
+
 
 class TimelineView(ScrollView):
     """ScrollView with a Timeline and its cursor."""
-    charsheet = ObjectProperty()
-    keys = ListProperty()
-    field = StringProperty('value')
+    charsheet_item = ObjectProperty()
+    data_iter = ObjectProperty()
     orientation = OptionProperty(
         'lr-tb',
         options=['lr-tb', 'tb-lr', 'rl-tb', 'tb-rl',
@@ -219,6 +165,7 @@ class TimelineView(ScrollView):
     curs_b = BoundedNumericProperty(0., min=0., max=1.)
     curs_a = BoundedNumericProperty(1., min=0., max=1.)
     cursor_color = ReferenceListProperty(curs_r, curs_g, curs_b, curs_a)
+    cursor_tick = NumericProperty()
 
 
 class NameAndValue(BoxLayout):
@@ -335,3 +282,284 @@ class NameAndValue(BoxLayout):
                 self.referent_name, self.variable_name, branch, tick)
         raise ValueError(
             "Unknown referent type: {}".format(self.referent_type))
+
+
+class CharSheetItem(BoxLayout):
+    """Either a NameAndValue or a Timeline, and a toggle button to switch
+    between them."""
+    charsheet = ObjectProperty()
+    item_type = OptionProperty(
+        'character_stat',
+        options=['thing_loc', 'thing_stat', 'place_stat',
+                 'portal_orig', 'portal_dest', 'portal_stat',
+                 'character_stat'])
+    referent = StringProperty()
+    stat_value = StringProperty()
+    stat_name = StringProperty()
+    is_timeline = BooleanProperty()
+
+    def __init__(self, **kwargs):
+        super(CharSheetItem, self).__init__(**kwargs)
+        self._finalize()
+
+    def _finalize(self, *args):
+        if not (self.charsheet and self.idx):
+            Clock.schedule_once(self.finalize, 0)
+            return
+        if self.is_timeline:
+            self.add_widget(self._mk_timeline())
+        else:
+            self.add_name_and_value()
+        self.add_toggle()
+
+    def _thing_loc_data_iter(self, branch, min_tick, max_tick):
+        innit = self.charsheet.facade.iter_thing_loc_bones(
+            thing=self.referent, branch=branch,
+            branch_from=branch, branch_to=branch,
+            tick_from=min_tick, tick_to=max_tick)
+        prev_bone = next(innit)
+        for bone in innit:
+            # from, to, value
+            yield (prev_bone.tick, bone.tick, prev_bone.location)
+
+    def _thing_stat_data_iter(self, branch, min_tick, max_tick):
+        innit = self.charsheet.facade.iter_thing_stat_bones(
+            thing=self.referent, stat=self.stat_name,
+            branch_from=branch, branch_to=branch,
+            tick_from=min_tick, tick_to=max_tick)
+        prev_bone = next(innit)
+        for bone in innit:
+            yield (prev_bone.tick, bone.tick,
+                   getattr(prev_bone, self.stat_name))
+
+    def _place_stat_data_iter(self, branch, min_tick, max_tick):
+        innit = self.charsheet.facade.iter_place_stat_bones(
+            place=self.referent, stat=self.stat_name,
+            branch_from=branch, branch_to=branch,
+            tick_from=min_tick, tick_to=max_tick)
+        prev_bone = next(innit)
+        for bone in innit:
+            yield (prev_bone.tick, bone.tick,
+                   getattr(prev_bone, self.stat_name))
+
+    def _portal_orig_data_iter(self, branch, min_tick, max_tick):
+        innit = self.charsheet.facade.iter_portal_loc_bones(
+            portal=self.referent,
+            branch_from=branch, branch_to=branch,
+            tick_from=min_tick, tick_to=max_tick)
+        prev_bone = next(innit)
+        for bone in innit:
+            yield (prev_bone.tick, bone.tick, prev_bone.origin)
+
+    def _portal_dest_data_iter(self, branch, min_tick, max_tick):
+        innit = self.charsheet.facade.iter_portal_loc_bones(
+            portal=self.referent,
+            branch_from=branch, branch_to=branch,
+            tick_from=min_tick, tick_to=max_tick)
+        prev_bone = next(innit)
+        for bone in innit:
+            yield (prev_bone.tick, bone.tick, prev_bone.destination)
+
+    def _portal_stat_data_iter(self, branch, min_tick, max_tick):
+        innit = self.charsheet.facade.iter_portal_stat_bones(
+            portal=self.referent, stat=self.stat_name,
+            branch_from=branch, branch_to=branch,
+            tick_from=min_tick, tick_to=max_tick)
+        prev_bone = next(innit)
+        for bone in innit:
+            yield (prev_bone.tick, bone.tick,
+                   getattr(prev_bone, self.stat_name))
+
+    def _character_stat_data_iter(self, branch, min_tick, max_tick):
+        innit = self.charsheet.facade.iter_stat_bones(
+            branch_from=branch, branch_to=branch,
+            tick_from=min_tick, tick_to=max_tick)
+        prev_bone = next(innit)
+        for bone in innit:
+            yield (prev_bone.tick, bone.tick,
+                   getattr(prev_bone, self.stat_name))
+
+    def _mk_timeline(self):
+        return TimelineView(
+            charsheet_item=self,
+            data_iter={
+                'thing_loc': self._thing_loc_data_iter,
+                'thing_stat': self._thing_stat_data_iter,
+                'place_stat': self._place_stat_data_iter,
+                'portal_orig': self._portal_orig_data_iter,
+                'portal_dest': self._portal_dest_data_iter,
+                'portal_stat': self._portal_stat_data_iter,
+                'character_stat': self._character_stat_data_iter
+            }[self.item_type])
+
+
+class CharSheet(StackLayout):
+    """The visual representation of a Facade.
+
+    Consists of a list of key-value pairs displayed in CharSheetItems,
+    which may look like cells in a table, or may look like timelines
+    showing the values for the key in the past and future.
+
+    """
+    facade = ObjectProperty()
+    _character_data = ListProperty()
+    _thing_data = ListProperty()
+    _place_data = ListProperty()
+    _portal_data = ListProperty()
+    timelineness = DictProperty(
+        {"thing_loc": set(),
+         "thing_stat": set(),
+         "place_stat": set(),
+         "portal_orig": set(),
+         "portal_dest": set(),
+         "portal_stat": set(),
+         "character_stat": set()})
+
+    def __init__(self, **kwargs):
+        self._trigger_refresh = Clock.create_trigger(self.refresh)
+        super(CharSheet, self).__init__(**kwargs)
+        self.finalize()
+
+    def finalize(self, *args):
+        if not self.facade:
+            Clock.schedule_once(self.finalize, 0)
+            return
+        self._trigger_refresh()
+
+    def refresh(self, *args):
+        old_data = [list(self._thing_data),
+                    list(self._place_data),
+                    list(self._portal_data)]
+        self._redata()
+        if [self._thing_data, self._place_data, self._portal_data] != old_data:
+            self.clear_widgets()
+            _ = lambda x: x
+            if len(self._character_data) > 0:
+                for (key, value) in self._character_data:
+                    self.add_widget(self._mk_cs_item(
+                        "character_stat", self.facade, key, value))
+            if len(self._thing_data) > 0:
+                self.add_widget(ClosetLabel(
+                    closet=self.facade.closet,
+                    stringname=_("Things:")))
+                for (thing, data) in self._thing_data:
+                    # these should really have different format than
+                    # the headers
+                    self.add_widget(ClosetLabel(
+                        closet=self.facade.closet,
+                        stringname=unicode(thing)))
+                    for (key, value) in data:
+                        self.add_widget(self._mk_cs_item(
+                            "thing_loc" if key == "location" else "thing_stat",
+                            thing, key, value))
+            if len(self._place_data) > 0:
+                self.add_widget(ClosetLabel(
+                    closet=self.facade.closet,
+                    stringname=_("Places")))
+                for (place, data) in self._place_data:
+                    self.add_widget(ClosetLabel(
+                        closet=self.facade.closet,
+                        stringname=unicode(place)))
+                    for (key, value) in data:
+                        self.add_widget(self._mk_cs_item(
+                            "place_stat", place, key, value))
+            if len(self._portal_data) > 0:
+                self.add_widget(ClosetLabel(
+                    closet=self.facade.closet,
+                    stringname=_("Portals")))
+                for (portal, data) in self._portal_data:
+                    self.add_widget(ClosetLabel(
+                        closet=self.facade.closet,
+                        stringname=unicode(portal)))
+                    for (key, value) in data:
+                        if key == "origin":
+                            a = "portal_orig"
+                        elif key == "destination":
+                            a = "portal_dest"
+                        else:
+                            a = "portal_stat"
+                        self.add_widget(self._mk_cs_item(
+                            a, portal, key, value))
+
+    def _redata(self):
+        _ = lambda x: x
+        (branch, tick) = self.facade.closet.time
+
+        def iter_thing_stat_bones(thing):
+            for stat_bone in self.facade.iter_thing_stat_bones(
+                    unicode(thing),
+                    branch_from=branch, branch_to=branch,
+                    tick_from=tick, tick_to=tick):
+                yield stat_bone
+
+        def mk_thing_data(thing):
+            return (
+                unicode(thing),
+                [(_("location"), self.facade.get_thing_location(
+                    thing, branch, tick))] + [
+                    (bone.key, bone.value) for bone in
+                    iter_thing_stat_bones(thing)])
+
+        self._thing_data = [
+            mk_thing_data(thing) for thing in
+            self.facade.iter_things(branch, tick)]
+
+        def iter_place_stat_bones(place):
+            for stat_bone in self.facade.iter_place_stat_bones(
+                    unicode(place),
+                    branch_from=branch,
+                    branch_to=branch,
+                    tick_from=tick,
+                    tick_to=tick):
+                yield stat_bone
+
+        def mk_place_data(place):
+            return (unicode(place), [(bone.key, bone.value)
+                                     for bone in iter_place_stat_bones])
+
+        self._character_data = [
+            (bone.key, bone.value) for bone in
+            self.facade.iter_stat_bones(
+                branch_from=branch,
+                branch_to=branch,
+                tick_from=tick,
+                tick_to=tick)]
+
+        self._place_data = [
+            mk_place_data(place)
+            for place in self.facade.iter_places(branch, tick)]
+
+        def iter_portal_stat_bones(portal):
+            for stat_bone in self.facade.iter_portal_stat_bones(
+                    unicode(portal),
+                    branch_from=branch,
+                    branch_to=branch,
+                    tick_from=tick,
+                    tick_to=tick):
+                yield stat_bone
+
+        def mk_portal_data(portal):
+            locb = self.facade.get_portal_loc_bone(portal, branch, tick)
+            return (
+                unicode(portal),
+                [(_("origin"), locb.origin),
+                 (_("destination"), locb.destination)] + [
+                    (bone.key, bone.value) for bone in
+                    iter_portal_stat_bones(portal)])
+
+        self._portal_data = [
+            mk_portal_data(portal) for portal in
+            self.facade.iter_portals(branch, tick)]
+
+    def _mk_cs_item(self, item_type, item_name, key, value):
+        assert(item_type in (
+            "thing_loc", "thing_stat", "place_stat", "portal_orig",
+            "portal_dest", "portal_stat", "character_stat"))
+        is_timeline = item_name in self.timelineness[item_type]
+        return CharSheetItem(
+            charsheet=self,
+            item_type=item_type,
+            referent=item_name,
+            is_timeline=is_timeline,
+            stat_name=key,
+            stat_value=value)
