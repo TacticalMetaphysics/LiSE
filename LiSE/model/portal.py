1c72039fa994b7d03c8cc73c4816ad12ad49cc17
Zachary Spector <zacharyspector@gmail.com>
2014-06-03 18:43:53 -0400

    Portals no longer have names, are identified only by start and end places.

--- LiSE/model/portal.py
+++ LiSE/model/portal.py
@@ -6,61 +6,75 @@ from LiSE.util import upbranch
 
 class Portal(Container):
     tables = [
-        ("portal_loc", {
-            "columns": {
-                "character": "text not null",
-                "name": "text not null",
-                "branch": "integer not null",
-                "tick": "integer not null",
-                "origin": "text",
-                "destination": "text"},
-            "primary_key": (
-                "character", "name", "branch", "tick")}),
-        ("portal_stat", {
-            "columns": {
-                "character": "text not null",
-                "name": "text not null",
-                "key": "text",
-                "branch": "integer not null default 0",
-                "tick": "integer not null default 0",
-                "value": "text"},
-            "primary_key": (
-                "character", "name", "key", "branch", "tick"),
-            "foreign_keys": {
-                "character, name": ("portal_loc", "character, name")}})]
+        (
+            "portal_stat",
+            {
+                "columns":
+                {
+                    "character": "text not null",
+                    "origin": "text not null",
+                    "destination": "text not null",
+                    "key": "text not null default 'exists'",
+                    "branch": "integer not null default 0",
+                    "tick": "integer not null default 0",
+                    "value": "text",
+                    "type": "text not null default 'text'"},
+                "primary_key":
+                (
+                    "character",
+                    "origin",
+                    "destination",
+                    "key",
+                    "branch",
+                    "tick"
+                ),
+                "checks":
+                ["type in ('text', 'real', 'boolean', 'integer')"]
+            }
+        )
+    ]
 
-    def __init__(self, character, name):
+    def __init__(self, character, origin, destination):
         self.character = character
-        self.name = name
+        self.origin = origin
+        self.destination = destination
 
     def __eq__(self, other):
         return (
-            isinstance(other, Portal) and
+            hasattr(other, 'character') and
+            hasattr(other, 'origin') and
+            hasattr(other, 'destination') and
             self.character == other.character and
-            self.name == other.name)
+            self.origin == other.origin and
+            self.destination == other.destination
+        )
 
     def __hash__(self):
-        return hash((self.character, self.name))
+        return hash((self.character, self.origin, self.destination))
 
     def __str__(self):
-        return str(self.name)
+        return "{}->{}".format(self.origin, self.destination)
 
     def __unicode__(self):
-        return unicode(self.name)
+        return u"{}->{}".format(self.origin, self.destination)
 
     def __repr__(self):
-        bone = self.get_loc_bone()
-        return "{2}({0}->{1})".format(
-            bone.origin, bone.destination, self.name)
+        return "Portal({0}->{1})".format(
+            self.origin,
+            self.destination
+        )
 
-    def __getitem__(self, key):
-        bone = self.loc_bone
-        return self.character.graph[bone.origin][bone.destination][key]
+    @property
+    def origplace(self):
+        return self.character.get_place(self.origin)
+
+    @property
+    def destplace(self):
+        return self.character.get_place(self.destination)
 
     @property
     def contents(self):
-        bone = self.loc_bone
-        return self.character.graph[bone.origin][bone.destination]['contents']
+        return self.character.graph[self.origin][self.destination]['contents']
 
     def _get_end(self, bprop):
         bone = self.loc_bone
@@ -77,15 +91,19 @@ class Portal(Container):
     def destination(self):
         return self._get_end('destination')
 
-    @property
-    def loc_bone(self):
-        (branch, tick) = self.character.closet.time
-        return self.character.closet.skeleton[u'portal_loc'][
-            self.character.name][self.name][branch].value_during(tick)
-
     def new_branch(self, parent, branch, tick):
-        skel = self.character.closet.skeleton[u"portal_loc"][
-            unicode(self.character)][unicode(self)][parent]
+        skel = (
+            self.character.closet.skeleton
+            [u"portal_stat"]
+            [unicode(self.character)]
+            [unicode(self.origin)]
+            [unicode(self.destination)]
+            [parent]
+        )
         for bone in upbranch(
-                self.character.closet, skel.iterbones(), branch, tick):
+                self.character.closet,
+                skel.iterbones(),
+                branch,
+                tick
+        ):
             yield bone
