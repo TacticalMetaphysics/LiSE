da7526ccb538baa7d66f56d2df34d9ae9a450850
Zachary Spector <zacharyspector@gmail.com>
2014-06-08 13:30:27 -0400

    new style __init__.py

--- LiSE/__init__.py
+++ LiSE/__init__.py
@@ -1,4 +1,4 @@
 # This file is part of LiSE, a framework for life simulation games.
 # Copyright (c) 2013 Zachary Spector,  zacharyspector@gmail.com
-__all__ = [
-    "model", "gui", "util", "closet"]
+import model, util
+__all__ = [model, util]
