a64c71a1edc926350760f531e9acb087dd0f7abd
Zachary Spector <zacharyspector@gmail.com>
2014-08-05 19:21:57 -0400

    make ELiDE launchable as python -m ELiDE

---
+++ setup_ELiDE.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name="ELiDE",
+    version="0.0",
+    packages=["ELiDE"],
+    install_requires=["LiSE", "kivy"],
+    author = "Zachary Spector",
+    author_email = "zacharyspector@gmail.com",
+    description = "Graphical developer's toolkit for LiSE",
+    license = "GPL3",
+    keywords = "game simulation IDE",
+    url = "https://github.com/LogicalDash/LiSE"
+)
