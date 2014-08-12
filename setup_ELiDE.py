a4936d45c264ee5a385e6610e77bba7938dab39e
Zachary Spector <zacharyspector@gmail.com>
2014-08-12 09:20:20 -0400

    include proper data files with ELiDE

--- setup_ELiDE.py
+++ setup_ELiDE.py
@@ -3,12 +3,23 @@ from setuptools import setup
 setup(
     name="ELiDE",
     version="0.0",
-    packages=["ELiDE"],
+    packages=["ELiDE", "ELiDE.board", "kivy.garden.stiffscroll"],
+    package_dir={'kivy.garden.stiffscroll': 'ELiDE/libs/garden/garden.stiffscroll'},
     install_requires=["LiSE", "kivy"],
     author = "Zachary Spector",
     author_email = "zacharyspector@gmail.com",
     description = "Graphical developer's toolkit for LiSE",
     license = "GPL3",
     keywords = "game simulation IDE",
-    url = "https://github.com/LogicalDash/LiSE"
+    url = "https://github.com/LogicalDash/LiSE",
+    package_data = {
+        "ELiDE": [
+            "elide.kv",
+            "assets/*.png",
+            "assets/*.jpg",
+            "assets/*.ttf",
+            "assets/*.atlas",
+            "assets/rltiles/*"
+        ]
+    }
 )
