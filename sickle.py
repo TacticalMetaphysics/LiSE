b31bd917a183bcd2038bbb40817eeeda94d6f85e
Zachary Spector <zacharyspector@gmail.com>
2014-08-21 18:09:11 -0400

    well, quite a lot of changes to this test case

--- sickle.py
+++ sickle.py
@@ -3,6 +3,7 @@ from LiSE import Engine
 from os import remove
 from cProfile import run
 
+
 def clear_off():
     for fn in ('LiSEworld.db', 'LiSEcode.db'):
         try:
@@ -10,20 +11,24 @@ def clear_off():
         except OSError:
             pass
 
-def mkengine(w='LiSEworld.db'):
+
+def mkengine(w='LiSEworld.db', caching=True, seed=None):
     return Engine(
         worlddb=w,
-        codedb='LiSEcode.db'
+        codedb='LiSEcode.db',
+        caching=caching,
+        random_seed=seed
     )
+
 
 def sickle_cell_test(
         engine,
         n_creatures=5,
+        n_sickles=3,
         malaria_chance=.05,
-        migrate_chance=.5,
-        mate_chance=.1,
-        mapsize=(6, 6),
-        startpos=(3, 3),
+        mate_chance=.05,
+        mapsize=(1, 1),
+        startpos=(0, 0),
         ticks=100
 ):
     """Natural Selection on Sickle Cell Anemia
@@ -40,13 +45,13 @@ def sickle_cell_test(
     species = engine.new_character(
         "species",
         mate_chance=mate_chance,
-        migrate_chance=migrate_chance,
-        n_creatures=n_creatures
+        malaria_chance=malaria_chance,
+        n_creatures=n_creatures,
     )
     for n in range(0, n_creatures):
         name = "critter" + str(n)
         stats = {
-            'sickle_a': True,
+            'sickle_a': n < n_sickles,
             'sickle_b': False,
             'male': engine.coinflip(),
             'last_mate_tick': -1
@@ -63,6 +68,16 @@ def sickle_cell_test(
             assert(thing[stat] == stats[stat])
         species.add_avatar("physical", name)
 
+
+# putting dieoff earlier in the code than mate means that dieoff will
+# be followed before mate is
+    @species.avatar.rule
+    def dieoff(engine, character, critter):
+        critter.delete()
+        assert(not critter.exists)
+        assert(critter.name not in critter.character.thing)
+        return critter['from_malaria']
+
     @species.avatar.rule
     def mate(engine, character, critter):
         """If I share my location with another critter, attempt to mate"""
@@ -71,6 +86,7 @@ def sickle_cell_test(
             oc for oc in critter.location.contents()
             if oc['male'] != critter['male']
         )
+        assert(len(suitors) > 0)
         other_critter = engine.choice(suitors)
         sickles = [
             critter['sickle_a'],
@@ -90,7 +106,8 @@ def sickle_cell_test(
             last_mate_tick=engine.tick
         )
         species.add_avatar("physical", name)
-        critter['last_mate_tick'] = other_critter['last_mate_tick'] = engine.tick
+        critter['last_mate_tick'] = other_critter['last_mate_tick'] =\
+            engine.tick
         n += 1
         return n
 
@@ -105,13 +122,65 @@ def sickle_cell_test(
                 return True
         return False
 
-    @mate.prereq
+    @mate.trigger
     def in_the_mood(engine, character, critter):
         return engine.random() < character.stat['mate_chance']
 
+    @dieoff.trigger
+    def sickle2(engine, character, critter):
+        r = critter['sickle_a'] and critter['sickle_b']
+        if r:
+            critter['from_malaria'] = False
+        return r
+
+    @dieoff.trigger
+    def malaria(engine, character, critter):
+        r = (
+            engine.random() < character.stat['malaria_chance'] and not
+            (critter['sickle_a'] or critter['sickle_b'])
+        )
+        if r:
+            critter['from_malaria'] = True
+        return r
+
+    print(
+        "Starting with {} creatures, of which {} have "
+        "at least one sickle betaglobin.".format(
+            len(species.avatar['physical']),
+            sum(
+                1 for critter in species.avatar['physical'].values()
+                if critter['sickle_a'] or critter['sickle_b']
+            )
+        )
+    )
+
     for i in range(0, ticks):
-        print(engine.next_tick())
+        r = engine.next_tick()
+        print("On tick {}, {} critters were born; "
+              "{} died of malaria, and {} of sickle cell anemia, "
+              "leaving {} alive.".format(
+                  i,
+                  sum(tup[0][0] for tup in r if tup[0] and tup[1] == 'mate'),
+                  sum(
+                      1 for tup in r if tup[0] and
+                      tup[0][0] and tup[1] == 'dieoff'
+                  ),
+                  sum(
+                      1 for tup in r if tup[0] and not
+                      tup[0][0] and tup[1] == 'dieoff'
+                  ),
+                  len(engine.character['species'].avatar['physical'])
+              ))
+    print(
+        "Of the remaining {} creatures, {} have a sickle betaglobin.".format(
+            len(species.avatar['physical']),
+            sum(
+                1 for critter in species.avatar['physical'].values()
+                if critter['sickle_a'] or critter['sickle_b']
+            )
+        )
+    )
 
 clear_off()
-with mkengine(':memory:') as engine:
+with mkengine('LiSEworld.db', seed=69105) as engine:
     run('sickle_cell_test(engine)')
