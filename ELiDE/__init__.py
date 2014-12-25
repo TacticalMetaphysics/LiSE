377924d20586e576b1b92b9b9c7ccd09881903fb
Zachary Spector <zacharyspector@gmail.com>
2014-12-25 15:35:40 -0500

    might be necessary to make the kv load properly

--- ELiDE/__init__.py
+++ ELiDE/__init__.py
@@ -2,9 +2,10 @@
 # Copyright (c) 2013-2014 Zachary Spector,  zacharyspector@gmail.com
 from .board import Board
 from .statgrid import StatListView
-from .menu import MenuIntInput, MenuTextInput
+from .menu import MenuTextInput, MenuIntInput
 from .pallet import Pallet
+from .configurator import SpotConfigDialog, PawnConfigDialog
 from .app import ELiDEApp
 
 
-__all__ = [Board, StatListView, MenuIntInput, MenuTextInput, Pallet, ELiDEApp]
+__all__ = [Board, StatListView, MenuTextInput, MenuIntInput, Pallet, SpotConfigDialog, PawnConfigDialog, ELiDEApp]
