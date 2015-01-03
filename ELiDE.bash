078420379c35a94e80d9992ca23a4176121ff1e6
Zachary Spector <zacharyspector@gmail.com>
2015-01-02 23:22:42 -0500

    I SAID NO PPAs

--- ELiDE.bash
+++ ELiDE.bash
@@ -16,8 +16,6 @@ else
         rm -rf "$LISE_PATH";
     fi;
 echo "About to install dependencies." &&
-sudo add-apt-repository -y ppa:thopiekar/pygame &&
-sudo add-apt-repository -y ppa:kivy-team/kivy-daily &&
 sudo apt-get -y update &&
 sudo apt-get -y install git cython3 python3-setuptools python3-kivy python3-numpy python3-networkx libsdl2-dev libsdl2-image-dev libsdl2-ttf-dev libsdl2-mixer-dev;
 
