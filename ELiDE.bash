0f84ee5142be6c289622e2d641d7884342f2f812
Zachary Spector <zacharyspector@gmail.com>
2015-01-02 22:45:25 -0500

    Apparently it's not safe to assume that the machine will have xterm, so I'll assume I'm being run interactively instead

--- ELiDE.bash
+++ ELiDE.bash
@@ -1,5 +1,4 @@
 #!/bin/bash
-export SHELL=/bin/bash;
 if [ -z "$LISE_PATH" ]; then
     LISE_PATH="`dirname "$0"`/LiSE";
     export LISE_PATH;
@@ -16,13 +15,11 @@ else
         # clean out failed installation
         rm -rf "$LISE_PATH";
     fi;
-    xterm -e 'bash -s <<EOF
 echo "About to install dependencies." &&
 sudo add-apt-repository -y ppa:thopiekar/pygame &&
 sudo add-apt-repository -y ppa:kivy-team/kivy-daily &&
 sudo apt-get -y update &&
 sudo apt-get -y install git cython3 python3-setuptools python3-kivy python3-numpy python3-networkx;
-EOF'
 
 
 
