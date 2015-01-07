0877b8d2943705d83c459aef774d25a86f4fab80
Zachary Spector <zacharyspector@gmail.com>
2015-01-07 09:57:03 -0500

    Try to support other distros in a simple way

--- ELiDE.bash
+++ ELiDE.bash
@@ -14,10 +14,11 @@ else
         # clean out failed installation
         rm -rf "$LISE_PATH";
     fi;
-
-    mkfifo announce;
-    mkfifo addapt;
-    echo '
+    if [ -n "`uname -v | grep Ubuntu`" ]; then
+        # on Ubuntu-derived distros, install from PPA
+        mkfifo announce;
+        mkfifo addapt;
+        echo '
 echo "About to install dependencies. This involves setting up two PPAs.";
 echo "ppa:thopiekar/pygame";
 sudo add-apt-repository -y ppa:thopiekar/pygame;
@@ -30,27 +31,62 @@ sudo apt-get -y install git cython3 python3-dev python3-setuptools python3-kivy;
 echo "All dependencies installed." >announce;
 sleep 1;
 exit;' >addapt &
-    MYTERM="`which gnome-terminal` -x";
-    if [ "$MYTERM" == " -x" ]; then
-        MYTERM="`which xfce4-terminal` -x";
-    fi;
-    if [ "$MYTERM" == " -x" ]; then
-        MYTERM="`which lxterminal` -e";
-    fi;
-    if [ "$MYTERM" == " -e" ]; then
-        MYTERM="`which konsole` -e";
-    fi;
-    if [ "$MYTERM" == " -e" ]; then
-        MYTERM="`which xterm` -e" ;
-    fi;
-    if [ "$MYTERM" == " -e" ]; then
-        MYTERM="";
-    fi;
-    $MYTERM bash --rcfile addapt;
+        MYTERM="`which gnome-terminal` -x";
+        if [ "$MYTERM" == " -x" ]; then
+            MYTERM="`which xfce4-terminal` -x";
+        fi;
+        if [ "$MYTERM" == " -x" ]; then
+            MYTERM="`which lxterminal` -e";
+        fi;
+        if [ "$MYTERM" == " -e" ]; then
+            MYTERM="`which konsole` -e";
+        fi;
+        if [ "$MYTERM" == " -e" ]; then
+            MYTERM="`which xterm` -e" ;
+        fi;
+        if [ "$MYTERM" == " -e" ]; then
+            MYTERM="";
+        fi;
+        $MYTERM bash --rcfile addapt;
 
-    echo <announce;
-    rm addapt;
-    rm announce;
+        echo <announce;
+        rm addapt;
+        rm announce;
+    else
+        VENV=""
+        if [ -n "`which virtualenv`" ]; then
+            VENV="virtualenv -p python3";
+        fi;
+        if [ -n "`which pyvenv`" ]; then
+            VENV=pyvenv;
+        fi;
+        if [ -z "$VENV" ]; then
+            echo "Couldn't find a usable virtualenv binary. Please install python3-venv and retry.";
+            exit 1;
+        fi;
+        if [ -z "`which git`" ]; then
+            echo "Couldn't find git. Please install it and retry."
+            exit 1;
+        fi;
+        echo "Creating LiSE-virtualenv";
+        $VENV LiSE-virtualenv;
+        echo "Activating LiSE-virtualenv";
+        source LiSE-virtualenv/bin/activate;
+        echo "Installing Cython";
+        pip3 install cython;
+        echo "Getting latest Kivy master";
+        git clone https://github.com/kivy/kivy.git kivy;
+        WORKINGDIR=$PWD;
+        cd kivy;
+        echo "Installing Kivy with SDL2 backend.";
+        echo "Assuming you have development headers for:";
+        echo "libsdl2";
+        echo "libsdl2-ttf";
+        echo "libsdl2-image";
+        echo "libsdl2-mixer";
+        USE_SDL2=1 python3 setup.py install;
+        cd $WORKINGDIR;
+    fi;
 
     git clone https://github.com/LogicalDash/LiSE.git "$LISE_PATH";
     cd "$LISE_PATH";
