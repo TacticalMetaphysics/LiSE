67e10a06aec8cc2aa4e68cf5a5da9a18b29a90a9
Zachary Spector <zacharyspector@gmail.com>
2015-01-13 19:30:32 -0500

    Revert "those are already defined on MirrorMapping"
    This reverts commit d0c9bf79fa00fab1b56c3b71823e1d40142f80eb.

--- ELiDE/statgrid.py
+++ ELiDE/statgrid.py
@@ -6,6 +6,9 @@ user. Autoupdates when there's a change for any reason.
 """
 from functools import partial
 from kivy.properties import (
+    NumericProperty,
+    StringProperty,
+    ReferenceListProperty,
     BooleanProperty,
     DictProperty,
     ObjectProperty
@@ -169,7 +172,8 @@ class StatListView(ListView, MirrorMapping):
         self.bind(mirror=self._trigger_sortkeys)
         self.bind(
             mirror=self._trigger_upd_data,
-            time=self._trigger_upd_data
+            branch=self._trigger_upd_data,
+            tick=self._trigger_upd_data
         )
         self._listeners = {}
         super().__init__(**kwargs)
