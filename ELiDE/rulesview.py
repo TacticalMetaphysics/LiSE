9e5550f65bcc9497de91fd681f738fb027e6154e
Zachary Spector <zacharyspector@gmail.com>
2015-01-23 09:01:22 -0500

    Start developing RulesView.

---
+++ ELiDE/rulesview.py
@@ -0,0 +1,7 @@
+# This file is part of LiSE, a framework for life simulation games.
+# Copyright (C) 2013-2014 Zachary Spector, ZacharySpector@gmail.com
+"""Widget to enable browsing rules and the functions that make them."""
+from kivy.logger import Logger
+from kivy.uix.widget import Widget
+from kivy.uix.button import Button
+from kivy.properties import ObjectProperty
