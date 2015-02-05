2ba4eba2ce06e68457693cb0f6181bd785e435b6
Zachary Spector <zacharyspector@gmail.com>
2015-02-05 08:46:20 -0500

    strings editor can now save changes

--- ELiDE/stringinput.py
+++ ELiDE/stringinput.py
@@ -1,7 +1,12 @@
 # This file is part of LiSE, a framework for life simulation games.
 # Copyright (c) 2013-2014 Zachary Spector,  zacharyspector@gmail.com
+from functools import partial
 from kivy.clock import Clock
-from kivy.properties import StringProperty, NumericProperty
+from kivy.properties import (
+    AliasProperty,
+    NumericProperty,
+    StringProperty
+)
 from kivy.lang import Builder
 from kivy.uix.boxlayout import BoxLayout
 
@@ -12,19 +17,31 @@ class StringInput(BoxLayout):
     name = StringProperty()
     source = StringProperty()
 
-    def on_name(self, *args):
+    def _get_name(self):
         if 'stringname' not in self.ids:
-            Clock.schedule_once(self.on_name, 0)
-            return
-        if self.ids.stringname.text != self.name:
-            self.ids.stringname.text = self.name
+            return ''
+        return self.ids.stringname.text
 
-    def on_source(self, *args):
-        if 'string' not in self.ids:
-            Clock.schedule_once(self.on_source, 0)
+    def _set_name(self, v, *args):
+        if 'stringname' not in self.ids:
+            Clock.schedule_once(partial(self._set_name, v), 0)
             return
-        if self.ids.string.text != self.source:
-            self.ids.string.text = self.source
+        self.ids.stringname.text = v
+
+    name = AliasProperty(_get_name, _set_name)
+
+    def _get_source(self):
+        if 'string' not in self.ids:
+            return ''
+        return self.ids.string.text
+
+    def _set_source(self, v, *args):
+        if 'string' not in self.ids:
+            Clock.schedule_once(partial(self._set_source, v), 0)
+            return
+        self.ids.string.text = v
+
+    source = AliasProperty(_get_source, _set_source)
 
 
 kv = """
