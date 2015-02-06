0680880d16bea2265457706c34b40ac226ee72a9
Zachary Spector <zacharyspector@gmail.com>
2015-02-05 21:41:39 -0500

    refactor the strings editing window into its own widget

--- ELiDE/layout.py
+++ ELiDE/layout.py
@@ -97,6 +97,67 @@ class StatWindow(BoxLayout):
         self.layout._close_stat_cfg_but = close_cfg_but
 
 
+class StringsEdWindow(BoxLayout):
+    layout = ObjectProperty()
+
+    def __init__(self, **kwargs):
+        kwargs['orientation'] = 'vertical'
+        super().__init__(**kwargs)
+
+    def on_layout(self, *args):
+        if self.layout is None:
+            return
+        if self.canvas is None:
+            Clock.schedule_once(self.on_layout, 0)
+            return
+        strings_ed = StringsEditor(
+            table='strings',
+            store=self.layout.engine.string,
+            size_hint_y=0.95
+        )
+        self.add_widget(strings_ed)
+        addclosestr = BoxLayout(
+            orientation='horizontal',
+            size_hint_y=0.05
+        )
+        self.add_widget(addclosestr)
+        add_string_field = TextInput(
+            hint_text='New string name'
+        )
+        addclosestr.add_widget(add_string_field)
+
+        def add_string(*args):
+            strings_ed.save()
+            newname = add_string_field.text
+            add_string_field.text = ''
+            strings_ed.name = newname
+            strings_ed.source = ''
+            strings_ed._trigger_redata_reselect()
+
+        add_string_but = Button(
+            text='New',
+            on_press=add_string
+        )
+        addclosestr.add_widget(add_string_but)
+
+        def dismiss_str(*args):
+            strings_ed.save()
+            self.layout._popover.remove_widget(self)
+            self.layout._popover.dismiss()
+            del self.layout._popover
+
+        close_string_but = Button(
+            text='Close',
+            on_press=dismiss_str
+        )
+        addclosestr.add_widget(close_string_but)
+
+        self.layout._strings_ed = strings_ed
+        self.layout._add_string_field = add_string_field
+        self.layout._add_string_but = add_string_but
+        self.layout._close_string_but = close_string_but
+
+
 class ELiDELayout(FloatLayout):
     """A master layout that contains one board and some menus
     and charsheets.
@@ -149,40 +210,7 @@ class ELiDELayout(FloatLayout):
             branch=self.timeupd,
             tick=self.timeupd,
         )
-        self._strings_ed = StringsEditor(
-            table='strings',
-            store=self.engine.string,
-            size_hint_y=0.95
-        )
-        self._strings_ed_window = BoxLayout(orientation='vertical')
-        self._strings_ed_window.add_widget(self._strings_ed)
-        addclosestr = BoxLayout(orientation='horizontal', size_hint_y=0.05)
-        self._add_string_field = TextInput(hint_text='New string name')
-        addclosestr.add_widget(self._add_string_field)
-
-        def add_string(*args):
-            self._strings_ed.save_if_needed()
-            newname = self._add_string_field.text
-            self._add_string_field.text = ''
-            self._strings_ed.name = newname
-            self._strings_ed.source = ''
-            self._strings_ed._trigger_redata_reselect()
-
-        self._add_string_but = Button(
-            text='new',
-            on_press=add_string
-        )
-        addclosestr.add_widget(self._add_string_but)
-
-        def dismiss_str(*args):
-            self._strings_ed.save_if_needed()
-            self._popover.remove_widget(self._strings_ed_window)
-            self._popover.dismiss()
-            del self._popover
-
-        self._close_string_but = Button(text='Close', on_press=dismiss_str)
-        addclosestr.add_widget(self._close_string_but)
-        self._strings_ed_window.add_widget(addclosestr)
+        self._strings_ed_window = StringsEdWindow(layout=self)
 
         self._funcs_ed_window = BoxLayout(orientation='vertical')
 
