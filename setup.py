f764bf2e553ea7beaf40bfb85399dc11e13347a2
Zachary Spector <zacharyspector@gmail.com>
2015-08-06 14:42:02 -0400

    this is not a good way to handle one-shot setup

--- setup.py
+++
@@ -1,9 +0,0 @@
-# This file is part of LiSE, a framework for life simulation games.
-# Copyright (c) 2013-2014 Zachary Spector,  zacharyspector@gmail.com
-import setupLiSE
-import setupELiDE
-
-__all__ = [setupLiSE, setupELiDE]
-
-# This is really just a brief way of running the LiSE and ELiDE setup
-# scripts in that order.
