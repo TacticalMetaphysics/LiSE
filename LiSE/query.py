89cfdb2cb4b5fc4f1b8007085a4219b34e2c2005
Zachary Spector <zacharyspector@gmail.com>
2015-10-12 21:23:32 -0400

    Revert "I got triggers and actions mixed up in these queries"
    Because it appears that in fact I had them in the correct order,
    though SQLAlchemy does them in an unintuitive order.
    
    This reverts commit 4b3e8e5e526f1e2143c6b9943f02ecbf788c9511.

--- LiSE/query.py
+++ LiSE/query.py
@@ -138,7 +138,7 @@ class QueryEngine(gorm.query.QueryEngine):
     def set_rule_triggers(self, rule, triggers):
         (rule, triggers) = map(self.json_dump, (rule, triggers))
         try:
-            return self.sql('ins_rule', rule, triggers, '["list"]', '["list"]')
+            return self.sql('ins_rule', rule, '["list"]', '["list"]', triggers)
         except IntegrityError:
             return self.sql('upd_rule_triggers', triggers, rule)
 
@@ -164,7 +164,7 @@ class QueryEngine(gorm.query.QueryEngine):
     def set_rule_actions(self, rule, actions):
         (rule, actions) = map(self.json_dump, (rule, actions))
         try:
-            return self.sql('ins_rule', rule, '["list"]', '["list"]', actions)
+            return self.sql('ins_rule', rule, actions, '["list"]', '["list"]')
         except IntegrityError:
             return self.sql('upd_rule_actions', actions, rule)
 
