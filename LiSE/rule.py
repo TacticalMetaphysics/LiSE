bb2b7668daa2350b499a05cf452aeb129a671b7f
Zachary Spector <zacharyspector@gmail.com>
2015-10-21 10:53:07 -0400

    all py3 files are utf-8

--- LiSE/rule.py
+++ LiSE/rule.py
@@ -1,4 +1,3 @@
-# coding: utf-8
 # This file is part of LiSE, a framework for life simulation games.
 # Copyright (c) 2013-2014 Zachary Spector,  zacharyspector@gmail.com
 """ The fundamental unit of game logic, the Rule, and structures to
