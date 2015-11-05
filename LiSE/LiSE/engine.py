bba4e5c16a5596254813e004ed9d4dfd924e3685
Zachary Spector <zacharyspector@gmail.com>
2015-11-05 14:43:11 -0500

    make the next_tick_listener signature match that of time_listener

--- LiSE/LiSE/engine.py
+++ LiSE/LiSE/engine.py
@@ -861,7 +861,7 @@ class Engine(AbstractEngine, gORM):
         # The last element is always None, but is not a sentinel; any
         # rule may return None.
         for listener in self._next_tick_listeners:
-            listener(self.branch, self.tick, r)
+            listener(curtick[0], curtick[1], self.branch, self.tick, r)
         return r[:-1]
 
     def new_character(self, name, **kwargs):
