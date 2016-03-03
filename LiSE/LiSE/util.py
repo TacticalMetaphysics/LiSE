546a1422496889856941333edac1ddae83e4cc57
Zachary Spector <zacharyspector@gmail.com>
2016-03-03 16:04:05 -0500

    Make EntityStatAccessor, mainly for use in statesearch queries

--- LiSE/LiSE/util.py
+++ LiSE/LiSE/util.py
@@ -3,7 +3,8 @@
 """Common utility functions and data structures.
 
 """
-from operator import attrgetter
+from operator import attrgetter, add, sub, mul, pow, truediv, floordiv, mod
+from functools import partial
 
 
 def getatt(attribute_name):
@@ -66,3 +67,99 @@ def keycache_iter(keycache, branch, tick, get_iterator):
     if tick not in keycache[branch]:
         keycache[branch][tick] = set(get_iterator())
     yield from keycache[branch][tick]
+
+
+class EntityStatAccessor(object):
+    def __init__(self, entity, stat, engine=None, branch=None, tick=None, current=False, mungers=[]):
+        if engine is None:
+            engine = entity.engine
+        if branch is None:
+            branch = engine.branch
+        if tick is None:
+            tick = engine.tick
+        self.current = current
+        self.engine = engine
+        self.entity = entity
+        self.stat = stat
+        self.branch = branch
+        self.tick = tick
+        self.mungers = mungers
+
+    def get(self):
+        if self.current:
+            res = self.entity[self.stat]
+        else:
+            time = self.engine.time
+            self.engine.time = (self.branch, self.tick)
+            res = self.entity[self.stat]
+            self.engine.time = time
+        for munger in self.mungers:
+            res = munger(res)
+        return res
+
+    def __hash__(self):
+        return hash(self.get())
+
+    def __ne__(self, other):
+        return self.get() != other
+
+    def __str__(self):
+        return str(self.get())
+
+    def __repr__(self):
+        return "EntityStatAccessor({}[{}]{}), {} mungers".format(
+            self.entity,
+            self.stat,
+            "" if self.current else ", branch={}, tick={}".format(self.branch, self.tick),
+            len(self.mungers)
+        )
+
+    def __gt__(self, other):
+        return self.get() > other
+
+    def __ge__(self, other):
+        return self.get() >= other
+
+    def __lt__(self, other):
+        return self.get() < other
+
+    def __le__(self, other):
+        return self.get() <= other
+
+    def __eq__(self, other):
+        return self.get() == other
+
+    def munge(self, munger):
+        return EntityStatAccessor(
+            self.entity,
+            self.stat,
+            self.engine,
+            self.branch,
+            self.tick,
+            self.current,
+            self.mungers + [munger]
+        )
+
+    def __add__(self, other):
+        return self.munge(partial(add, other))
+
+    def __sub__(self, other):
+        return self.munge(partial(sub, other))
+
+    def __mul__(self, other):
+        return self.munge(partial(mul, other))
+
+    def __rpow__(self, other, modulo=None):
+        return self.munge(partial(pow, other, modulo=modulo))
+
+    def __rdiv__(self, other):
+        return self.munge(partial(truediv, other))
+
+    def __rfloordiv__(self, other):
+        return self.munge(partial(floordiv, other))
+
+    def __rmod__(self, other):
+        return self.munge(partial(mod, other))
+
+    def __getitem__(self, k):
+        return self.munge(lambda x: x[k])
