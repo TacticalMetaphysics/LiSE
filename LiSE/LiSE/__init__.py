8300b8046b7558e44e9bc7fed75931ec01cba3a9
Zachary Spector <zacharyspector@gmail.com>
2016-05-08 00:47:15 -0400

    Use a blank __init__.py for convenience importing submodules

--- LiSE/LiSE/__init__.py
+++
@@ -1,4 +0,0 @@
-# This file is part of LiSE, a framework for life simulation games.
-# Copyright (c) Zachary Spector,  zacharyspector@gmail.com
-"""LiSE, the Life Simulator Engine. A rules engine for games."""
-from .engine import Engine
