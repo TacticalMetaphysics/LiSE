7ff06d19d7157fa8a70a46de30993b6eb2bbcd80
Zachary Spector <zacharyspector@gmail.com>
2016-08-05 09:17:59 -0400

    Correct the KeyError message in AvatarMapProxy.__getitem__

--- LiSE/LiSE/proxy.py
+++ LiSE/LiSE/proxy.py
@@ -1068,7 +1068,7 @@ class AvatarMapProxy(Mapping):
 
     def __getitem__(self, k):
         if k not in self:
-            raise KeyError("{} has no avatar in {}".format(k, self.character.name))
+            raise KeyError("{} has no avatar in {}".format(self.character.name, k))
         return self.GraphAvatarsProxy(self.character, self.character.engine.character[k])
 
     def __getattr__(self, attr):
