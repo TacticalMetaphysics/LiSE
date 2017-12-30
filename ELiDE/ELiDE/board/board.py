60f04adfa1c79609134cce23e4f043ae9099e16b
Zachary Spector <zacharyspector@gmail.com>
2017-12-30 08:57:13 -0500

    Fix some weirdness when scrolling along the upper or right edges of the board

--- ELiDE/ELiDE/board/board.py
+++ ELiDE/ELiDE/board/board.py
@@ -986,16 +986,16 @@ class BoardScatterPlane(ScatterPlane):
 
     def apply_transform(self, trans, post_multiply=False, anchor=(0, 0)):
         super().apply_transform(trans, post_multiply=post_multiply, anchor=anchor)
-        if self.board and (
-            self.to_parent(self.board.width, 0)[0] < self.parent.right or
-            self.to_parent(0, self.board.height)[1] < self.parent.top
-        ):
-            super().apply_transform(trans.inverse(), post_multiply, anchor)
+        self._last_transform = trans, post_multiply, anchor
 
     def on_transform_with_touch(self, touch):
         x, y = self.pos
         w = self.board.width * self.scale
         h = self.board.height * self.scale
+        if hasattr(self, '_last_transform') and (w < self.parent.width or h < self.parent.height):
+            trans, post_multiply, anchor = self._last_transform
+            super().apply_transform(trans.inverse(), post_multiply, anchor)
+            return
         if x > self.parent.x:
             self.x = self.parent.x
         if y > self.parent.y:
