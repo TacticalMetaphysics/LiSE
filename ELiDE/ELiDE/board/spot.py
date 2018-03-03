7430ac57154309d7fa2397ac9a8dd0667a6026f9
Zachary Spector <zacharyspector@gmail.com>
2018-03-03 15:36:46 -0500

    Move some Spot stuff to PawnSpot
    In preparation for adding Pawns as children of other Pawns.

--- ELiDE/ELiDE/board/spot.py
+++ ELiDE/ELiDE/board/spot.py
@@ -4,8 +4,6 @@
 top of these.
 
 """
-from functools import partial
-
 from kivy.clock import Clock
 
 from kivy.properties import (
@@ -54,51 +52,8 @@ class Spot(PawnSpot):
         super().__init__(**kwargs)
         self.bind(pos=self._trigger_upd_pawns_here)
 
-    @trigger
-    def restack(self, *args):
-        childs = sorted(list(self.children), key=lambda child: child.priority, reverse=True)
-        self.clear_widgets()
-        for child in childs:
-            self.add_widget(child)
-
     def on_board(self, *args):
         self.board.bind(size=self._upd_pos)
-
-    def _get_pospawn_partial(self, pawn):
-        if pawn not in self._pospawn_partials:
-            self._pospawn_partials[pawn] = partial(
-                self.pospawn, pawn
-            )
-        return self._pospawn_partials[pawn]
-
-    def _get_pospawn_trigger(self, pawn, *args):
-        if pawn not in self._pospawn_triggers:
-            self._pospawn_triggers[pawn] = Clock.create_trigger(
-                self._get_pospawn_partial(pawn)
-            )
-        return self._pospawn_triggers[pawn]
-
-    def bind_trigger_pospawn(self, pawn):
-        trigger = self._get_pospawn_trigger(pawn)
-        pawn.bind(
-            pos=trigger,
-            size=trigger
-        )
-        self.bind(
-            pos=trigger,
-            size=trigger
-        )
-
-    def unbind_trigger_pospawn(self, pawn):
-        trigger = self._get_pospawn_trigger(pawn)
-        pawn.unbind(
-            pos=trigger,
-            size=trigger
-        )
-        self.unbind(
-            pos=trigger,
-            size=trigger
-        )
 
     def _upd_pos(self, *args):
         if self.board is None:
@@ -124,77 +79,6 @@ class Spot(PawnSpot):
         self.proxy['_x'] = self.x / self.board.width
         self.proxy['_y'] = self.y / self.board.height
     _trigger_push_pos = trigger(push_pos)
-
-    def add_widget(self, wid, i=None, canvas=None):
-        """Put the widget's canvas in my ``board``'s ``pawnlayout`` rather
-        than my own canvas.
-
-        The idea is that all my child widgets are to be instances of
-        :class:`Pawn`, and should therefore be drawn after every
-        non-:class:`Pawn` widget, so that pawns are on top of spots
-        and arrows.
-
-        """
-        if i is None:
-            for i, child in enumerate(self.children, start=1):
-                if wid.priority < child.priority:
-                    i = len(self.children) - i
-                    break
-        super().add_widget(wid, i, canvas)
-        self.bind_trigger_pospawn(wid)
-        if not hasattr(wid, 'group'):
-            return
-        wid._no_use_canvas = True
-        mycanvas = (
-            self.canvas.after if canvas == 'after' else
-            self.canvas.before if canvas == 'before' else
-            self.canvas
-        )
-        pawncanvas = (
-            self.board.pawnlayout.canvas.after if canvas == 'after' else
-            self.board.pawnlayout.canvas.before if canvas == 'before' else
-            self.board.pawnlayout.canvas
-        )
-        mycanvas.remove(wid.canvas)
-        for child in self.children:
-            if hasattr(child, 'group'):
-                if child.group in pawncanvas.children:
-                    pawncanvas.remove(child.group)
-                pawncanvas.add(child.group)
-            else:
-                pawncanvas.add(child.canvas)
-        self.pospawn(wid)
-
-    def remove_widget(self, wid):
-        try:
-            self.unbind_trigger_pospawn(wid)
-        except KeyError:
-            pass
-        return super().remove_widget(wid)
-
-    def pospawn(self, pawn, *args):
-        """Given some :class:`Pawn` instance that's to be on top of me, set
-        its ``pos`` so that it looks like it's on top of me but
-        doesn't cover me so much that you can't select me.
-
-        """
-        i = 0
-        for child in self.children:
-            if child is pawn:
-                break
-            i += 1
-        off = i * self.offset
-        (x, y) = self.center
-        pawn.pos = (x+off, y+off)
-
-    def _upd_pawns_here(self, *args):
-        """Move any :class:`Pawn` atop me so it still *is* on top of me,
-        presumably after I've moved.
-
-        """
-        for pawn in self.children:
-            self.pospawn(pawn)
-    _trigger_upd_pawns_here = trigger(_upd_pawns_here)
 
     def on_touch_move(self, touch):
         """If I'm being dragged, move to follow the touch."""
