576464e1cf741966933fbb1114208a2e67ebfbd7
Zachary Spector <zacharyspector@gmail.com>
2018-03-21 22:25:37 -0400

    Fix a bunch of test utility stuff

--- LiSE/LiSE/test_character.py
+++ LiSE/LiSE/test_character.py
@@ -27,6 +27,11 @@ class CharacterSetStorageTest(CharacterTest, allegedb.test.SetStorageTest):
 def set_in_mapping(mapp, stat, v):
     # Mutate the stuff in-place instead of simply replacing it,
     # because this could trigger side effects
+    if stat == 'name':
+        return
+    if v is None:
+        del mapp[stat]
+        return
     if stat not in mapp:
         mapp[stat] = v
         return
@@ -50,16 +55,24 @@ def set_in_mapping(mapp, stat, v):
 
 
 def update_char(char, *, stat=(), node=(), portal=()):
-    end_stats = {'name': char.name}
+    def update(d, dd):
+        for k, v in dd.items():
+            if v is None and k in d:
+                del d[k]
+            else:
+                d[k] = v
+    end_stats = char.stat.unwrap()
     for stat, v in stat:
         set_in_mapping(char.stat, stat, v)
         if v is None and stat in end_stats:
             del end_stats[stat]
         else:
             end_stats[stat] = v
-    end_places = {}
-    end_things = {}
+    end_places = char.place.unwrap()
+    end_things = char.thing.unwrap()
     for node, v in node:
+        if 'name' not in v:
+            v['name'] = node
         if v is None:
             del char.node[node]
             if node in end_places:
@@ -67,25 +80,41 @@ def update_char(char, *, stat=(), node=(), portal=()):
             if node in end_things:
                 del end_things[node]
         elif node in char.place:
-            me = end_places[node] = dict(char.place[node])
             if 'location' in v:
-                del char.place[node]
                 del end_places[node]
-                char.thing[node] = me
-                continue
-            me.update(v)
-            for k, vv in v.items():
-                set_in_mapping(char.place[node], k, vv)
+                char.place2thing(node, v.pop('location'))
+                if node in end_places:
+                    me = end_things[node] = end_places.pop(node)
+                else:
+                    me = end_things[node] = dict(char.thing[node])
+                update(me, v)
+                for k, vv in v.items():
+                    set_in_mapping(char.thing[node], k, vv)
+            else:
+                if node in end_places:
+                    me = end_places[node]
+                else:
+                    me = end_places[node] = dict(char.place[node])
+                update(me, v)
+                for k, vv in v.items():
+                    set_in_mapping(char.place[node], k, vv)
         elif node in char.thing:
-            me = end_things[node] = dict(char.thing[node])
             if 'location' in v and v['location'] is None:
-                del char.thing[node]
-                del end_things[node]
-                char.place[node] = me
-                continue
-            me.update(v)
-            for k, vv in v.items():
-                set_in_mapping(char.thing[node], k, vv)
+                if node in end_things:
+                    me = end_places[node] = end_things.pop(node)
+                else:
+                    me = end_places[node] = dict(char.thing[node])
+                del me['location']
+                del v['location']
+                char.thing2place(node)
+                update(me, v)
+                for k, vv in v.items():
+                    set_in_mapping(char.place[node], k, vv)
+            else:
+                me = end_things[node] = dict(char.thing[node])
+                update(me, v)
+                for k, vv in v.items():
+                    set_in_mapping(char.thing[node], k, vv)
         elif 'location' in v:
             end_things[node] = v
             me = char.new_thing(node, v.pop('location'))
@@ -119,7 +148,7 @@ def update_char(char, *, stat=(), node=(), portal=()):
      {0: [1], 1: [0], 'kobold': []},
      {'spam': 'eggs', 'ham': {'baked beans': 'delicious'}, 'qux': ['quux', 'quuux'],
                              'clothes': {'hats', 'shirts', 'pants'}},
-     [('kobold', {'evil': True}), (0, {'evil': False}), (1, {'evil': False})],
+     [('kobold', {'location': 0, 'evil': True}), (0, {'evil': False}), (1, {'evil': False})],
      [('spam', None), ('qux', ['quux']), ('clothes', 'no')],
      [(2, {'evil': False}), ('kobold', {'evil': False})],
      [(0, 1, None), (0, 2, {'hi': 'hello'})]
@@ -136,13 +165,13 @@ def character_updates(request):
 
 def test_facade(character_updates):
     character, statup, nodeup, edgeup = character_updates
-    start_stat = dict(character.stat)
-    start_place = dict(character.place)
-    start_thing = dict(character.thing)
+    start_stat = character.stat.unwrap()
+    start_place = character.place.unwrap()
+    start_thing = character.thing.unwrap()
     start_edge = {}
     for o in character.edge:
         for d in character.edge[o]:
-            start_edge.setdefault(o, {})[d] = dict(character.edge[o][d])
+            start_edge.setdefault(o, {})[d] = character.edge[o][d].unwrap()
     facade = character.facade()
     updated = update_char(
         facade, stat=statup, node=nodeup, portal=edgeup)
@@ -151,9 +180,9 @@ def test_facade(character_updates):
     assert facade.thing == updated['thing']
     assert facade.portal == updated['portal']
     # changes to a facade should not impact the underlying character
-    assert start_stat == dict(character.stat)
-    assert start_place == dict(character.place)
-    assert start_thing == dict(character.place)
+    assert start_stat == character.stat.unwrap()
+    assert start_place == character.place.unwrap()
+    assert start_thing == character.thing.unwrap()
     end_edge = {}
     for o in character.edge:
         for d in character.edge[o]:
