ab2a9f9e9bcb1fcc3b68a0b9d0484bba3890272d
Zachary Spector <zacharyspector@gmail.com>
2018-06-10 10:17:04 -0400

    Test containment more

--- LiSE/LiSE/tests/test_thing.py
+++ LiSE/LiSE/tests/test_thing.py
@@ -9,9 +9,16 @@ def something():
 
 
 def test_container(something):
-    assert something.container == something.character.place['somewhere']
-    something.character.add_place('somewhere2')
-    something.character.add_portal('somewhere', 'somewhere2')
+    pl1 = something.character.place['somewhere']
+    assert something.container == pl1
+    pl2 = something.character.new_place('somewhere2')
+    po = something.character.new_portal('somewhere', 'somewhere2')
     something.next_location = 'somewhere2'
     assert something.container == something.character.portal['somewhere']['somewhere2']
     assert something.location == something.character.node['somewhere']
+    assert something.name in pl1.content
+    assert not something.name in pl2.content
+    assert [something] == list(pl1.contents())
+    assert [] == list(pl2.contents())
+    assert something.name in po.content
+    assert [something] == list(po.contents())
