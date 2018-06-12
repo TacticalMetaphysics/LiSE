5c6e98de1878f42362eaa73cec7c27822a6457e4
Zachary Spector <zacharyspector@gmail.com>
2018-06-12 10:11:35 -0400

    Use correct test data for test_load

--- allegedb/allegedb/tests/test_load.py
+++ allegedb/allegedb/tests/test_load.py
@@ -4,11 +4,15 @@ from allegedb import ORM
 import networkx as nx
 
 
+scalefreestart = nx.MultiDiGraph(name='scale_free_graph_22')
+scalefreestart.add_edges_from([(0, 1), (1, 2), (2, 0)])
+
+
 testgraphs = [
-    nx.chvatal_graph(),
-    nx.scale_free_graph(22),
-    nx.chordal_cycle_graph(22),
-    nx.path_graph(9, create_using=nx.MultiDiGraph())
+    nx.chvatal_graph(create_using=nx.Graph(name='chvatal_graph')),
+    nx.scale_free_graph(22, create_using=scalefreestart),
+    nx.chordal_cycle_graph(22, create_using=nx.MultiGraph(name='chordal_cycle_graph_22')),
+    nx.path_graph(9, create_using=nx.DiGraph(name='path_graph_9'))
 ]
 
 
