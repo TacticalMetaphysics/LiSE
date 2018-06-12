0669ac4dc9dbbbd723fc6c18ba60d093f02c8b25
Zachary Spector <zacharyspector@gmail.com>
2018-06-12 15:03:29 -0400

    Fix weird graph names in test_load

--- allegedb/allegedb/tests/test_load.py
+++ allegedb/allegedb/tests/test_load.py
@@ -9,11 +9,14 @@ scalefreestart.add_edges_from([(0, 1), (1, 2), (2, 0)])
 
 
 testgraphs = [
-    nx.chvatal_graph(create_using=nx.Graph(name='chvatal_graph')),
+    nx.chvatal_graph(),
     nx.scale_free_graph(2, create_using=scalefreestart),
     nx.chordal_cycle_graph(2, create_using=nx.MultiGraph(name='chordal_cycle_graph_2')),
-    nx.path_graph(9, create_using=nx.DiGraph(name='path_graph_9'))
 ]
+# have to name it after creation because it clears the create_using
+path_graph_9 = nx.path_graph(9)
+path_graph_9.name = 'path_graph_9'
+testgraphs.append(path_graph_9)
 
 
 @pytest.fixture
