8a0487eabccffe432972cef94acf6df1eba0d5f6
Zachary Spector <zacharyspector@gmail.com>
2018-06-12 14:45:27 -0400

    Make the output of the load test easier to understand

--- allegedb/allegedb/tests/test_load.py
+++ allegedb/allegedb/tests/test_load.py
@@ -4,14 +4,14 @@ from allegedb import ORM
 import networkx as nx
 
 
-scalefreestart = nx.MultiDiGraph(name='scale_free_graph_22')
+scalefreestart = nx.MultiDiGraph(name='scale_free_graph_2')
 scalefreestart.add_edges_from([(0, 1), (1, 2), (2, 0)])
 
 
 testgraphs = [
     nx.chvatal_graph(create_using=nx.Graph(name='chvatal_graph')),
-    nx.scale_free_graph(22, create_using=scalefreestart),
-    nx.chordal_cycle_graph(22, create_using=nx.MultiGraph(name='chordal_cycle_graph_22')),
+    nx.scale_free_graph(2, create_using=scalefreestart),
+    nx.chordal_cycle_graph(2, create_using=nx.MultiGraph(name='chordal_cycle_graph_2')),
     nx.path_graph(9, create_using=nx.DiGraph(name='path_graph_9'))
 ]
 
@@ -37,5 +37,7 @@ def db():
 def test_basic_load(db):
     for graph in testgraphs:
         alleged = db.graph[graph.name]
-        assert graph.node.keys() == alleged.node.keys()
-        assert graph.edges == alleged.edges
+        assert set(graph.node.keys()) == set(alleged.node.keys()), "{}'s nodes are not the same after load".format(
+            graph.name
+        )
+        assert set(graph.edges) == set(alleged.edges), "{}'s edges are not the same after load".format(graph.name)
