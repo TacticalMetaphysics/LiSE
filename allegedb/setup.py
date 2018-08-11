c2f30deacc621beefa434b33b313bf2861fd1311
Zachary Spector <zacharyspector@gmail.com>
2018-08-10 22:00:27 -0400

    Relicense everything under AGPL3+ with some additional permissions

--- allegedb/setup.py
+++ allegedb/setup.py
@@ -1,3 +1,18 @@
+# This file is part of allegedb, an object relational mapper for versioned graphs.
+# Copyright (C) Zachary Spector. public@zacharyspector.com
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
 from setuptools import setup
 setup(
     name = "allegedb",
@@ -7,7 +22,7 @@ setup(
     author = "Zachary Spector",
     author_email = "zacharyspector@gmail.com",
     description = "An object-relational mapper serving database-backed versions of the standard networkx graph classes.",
-    license = "BSD",
+    license = "AGPL3+",
     keywords = "orm graph networkx sql database",
     url = "https://github.com/LogicalDash/LiSE",
     package_dir={
