569e39a67cc0575b4a64a869e8e657ed9cf44281
Zachary Spector <zacharyspector@gmail.com>
2018-08-10 22:02:41 -0400

    Relicense everything under AGPL3+ with some additional permissions

--- ELiDE/setup.py
+++ ELiDE/setup.py
@@ -1,5 +1,18 @@
-# This file is part of LiSE, a framework for life simulation games.
-# Copyright (c) Zachary Spector,  zacharyspector@gmail.com
+# This file is part of ELiDE, frontend to LiSE, a framework for life simulation games.
+# Copyright (c) Zachary Spector, public@zacharyspector.com
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
 import sys
 if sys.version_info[0] < 3 or (
         sys.version_info[0] == 3 and sys.version_info[1] < 3
@@ -11,6 +24,7 @@ from setuptools import setup
 setup(
     name="ELiDE",
     version="0.8.0a",
+    license="AGPL3+",
     packages=[
         "ELiDE",
         "ELiDE.board",
--- LiSE/setup.py
+++ LiSE/setup.py
@@ -1,5 +1,18 @@
 # This file is part of LiSE, a framework for life simulation games.
-# Copyright (c) Zachary Spector,  zacharyspector@gmail.com
+# Copyright (c) Zachary Spector, public@zacharyspector.com
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
 import sys
 if sys.version_info[0] < 3 or (
         sys.version_info[0] == 3 and
@@ -16,7 +29,7 @@ setup(
     description="Rules engine for life simulation games",
     author="Zachary Spector",
     author_email="zacharyspector@gmail.com",
-    license="GPL3",
+    license="AGPL3+",
     keywords="game simulation",
     url="https://github.com/LogicalDash/LiSE",
     packages=[
