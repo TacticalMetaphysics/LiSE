8631ce1bff66e09e8c75919f5f004e8439910b76
Zachary Spector <public@zacharyspector.com>
2018-10-21 17:17:14 -0400

    Never mind the requirements.txt, just hack readthedocs with setup.py

--- setup.py
+++ setup.py
@@ -6,4 +6,48 @@ import sys
 sys.environ["NDKPLATFORM"] = 1
 sys.environ["LIBLINK"] = 1
 sys.environ["READTHEDOCS"] = 1
-setup(name="LiSE docs", version="0.9", license="AGPL3+", install_requires=["kivy>=1.10.0"])
+setup(
+    name="LiSE docs",
+    version="0.9",
+    license="AGPL3+",
+    packages=[
+        "allegedb",
+        "LiSE",
+        "ELiDE",
+        "ELiDE.board",
+        "ELiDE.kivygarden.texturestack"
+    ],
+    package_dir={
+        'allegedb': 'allegedb/allegedb',
+        'LiSE': 'LiSE/LiSE',
+        'ELiDE': 'ELiDE/ELiDE',
+        'ELiDE.board': 'ELiDE/ELiDE/board',
+        'ELiDE.kivygarden.texturestack':
+        'ELiDE/ELiDE/kivygarden/texturestack'
+    },
+    install_requires=[
+        "networkx>=1.9",
+        "astunparse>=1.5.0",
+        "u-msgpack-python>=2.4.1",
+        "blinker",
+        "numpy",
+        "kivy>=1.10.0",
+        "pygments"
+    ],
+    package_data={
+        "allegedb": [
+            "sqlite.json"
+        ],
+        "LiSE": [
+            "sqlite.json"
+        ],
+        "ELiDE": [
+            "assets/*.png",
+            "assets/*.jpg",
+            "assets/*.ttf",
+            "assets/*.atlas",
+            "assets/rltiles/*"
+        ]
+    },
+    zip_safe=False
+)
