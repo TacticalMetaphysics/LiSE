75a7ac3424037030db4c469af796e58f41a3283b
Zachary Spector <public@zacharyspector.com>
2020-03-24 17:56:02 -0400

    Move some utilities into tests/util.py

--- ELiDE/ELiDE/tests/util.py
+++ ELiDE/ELiDE/tests/util.py
@@ -1,3 +1,4 @@
+from blinker import Signal
 from kivy.input.motionevent import MotionEvent
 
 
@@ -7,3 +8,28 @@ class TestTouch(MotionEvent):
         self.sx = args['sx']
         self.sy = args['sy']
         super().depack(args)
+
+
+class ListenableDict(dict, Signal):
+    def __init__(self):
+        Signal.__init__(self)
+
+
+class MockEngine(Signal):
+    universal = ListenableDict()
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.turn = 0
+        self._ready = True
+
+    def __setattr__(self, key, value):
+        if not hasattr(self, '_ready'):
+            super().__setattr__(key, value)
+            return
+        self.send(self, key=key, value=value)
+        super().__setattr__(key, value)
+
+    def next_turn(self, *args, **kwargs):
+        self.turn += 1
+        kwargs['cb']('next_turn', 'master', self.turn, 0, ([], {}))
