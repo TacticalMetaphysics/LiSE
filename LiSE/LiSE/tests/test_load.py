bb1ce0800ec1e2b31b23521ec1ac98bab30340fd
Zachary Spector <public@zacharyspector.com>
2021-02-20 18:48:05 -0500

    Fix test_keyframe_load_init
    *Of course* I don't have an individual cache for each entity yet

--- LiSE/LiSE/tests/test_load.py
+++ LiSE/LiSE/tests/test_load.py
@@ -16,8 +16,6 @@ def test_keyframe_load_init(tempdir):
     eng.snap_keyframe()
     eng.close()
     eng = Engine(tempdir)
-    assert eng._things_cache.keyframe['physical', 'kobold'][
-        eng.branch][eng.turn][eng.tick]
     assert 'kobold' in eng.character['physical'].thing
     assert (0, 0) in eng.character['physical'].place
     assert (0, 1) in eng.character['physical'].portal[0, 0]
