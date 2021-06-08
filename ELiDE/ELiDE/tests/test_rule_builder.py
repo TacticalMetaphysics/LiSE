b25fa5f6ca26b26fd84e4d661d88b9300ed807f1
Zachary Spector <public@zacharyspector.com>
2021-06-07 23:12:09 -0400

    Refactor tests

--- ELiDE/ELiDE/tests/test_rule_builder.py
+++ ELiDE/ELiDE/tests/test_rule_builder.py
@@ -1,3 +1,4 @@
+from abc import abstractmethod
 from LiSE import Engine
 from LiSE.examples import kobold
 from LiSE.examples import polygons
@@ -5,28 +6,49 @@ from .util import idle_until, window_with_widget, ELiDEAppTest
 from ..card import Card
 
 
-class TestRuleBuilder(ELiDEAppTest):
-    def test_rule_builder_display(self):
+class RuleBuilderTest(ELiDEAppTest):
+    @abstractmethod
+    def install(self, engine: Engine):
+        raise NotImplementedError()
+
+    @abstractmethod
+    def get_selection(self):
+        raise NotImplementedError()
+
+    def setUp(self):
+        super(RuleBuilderTest, self).setUp()
         with Engine(self.prefix) as eng:
-            kobold.inittest(eng)
+            self.install(eng)
         app = self.app
         mgr = app.build()
-        win = window_with_widget(mgr)
+        self.win = window_with_widget(mgr)
         screen = app.mainscreen
         idle_until(lambda: app.rules.rulesview, 100, 'Never made rules view')
-        board = screen.graphboards['physical']
-        idle_until(lambda: 'kobold' in board.pawn, 100,
+        self.board = screen.graphboards['physical']
+        idle_until(lambda: 'kobold' in self.board.pawn, 100,
                    'never got the pawn for the kobold')
-        app.selection = board.pawn['kobold']
+        app.selection = self.get_selection()
         screen.charmenu.charmenu.toggle_rules()
         rules = app.rules
         rules_box = rules.children[0]
         idle_until(lambda: 'ruleslist' in rules_box.ids, 100,
                    'Never made rules list')
-        rules_list = rules_box.ids.ruleslist
-        rules_view = rules_box.ids.rulesview
+        self.rules_list = rules_list = rules_box.ids.ruleslist
+        self.rules_view = rules_box.ids.rulesview
         idle_until(lambda: rules_list.children[0].children, 100,
                    'Never filled rules list')
+
+
+class TestRuleBuilderKobold(RuleBuilderTest):
+    def install(self, engine: Engine):
+        kobold.inittest(engine)
+
+    def get_selection(self):
+        return self.board.pawn['kobold']
+
+    def test_rule_builder_display_trigger(self):
+        rules_list = self.rules_list
+        rules_view = self.rules_view
         idle_until(lambda: 'shrubsprint' in {
             rulebut.text for rulebut in rules_list.children[0].children}, 100,
                    'Never made shrubsprint button')
@@ -45,18 +67,24 @@ class TestRuleBuilder(ELiDEAppTest):
         assert card_names == {'standing_still', 'aware', 'uncovered',
                               'sametile', 'breakcover', 'kobold_alive'}
 
-    def test_char_rule_builder_display(self):
+
+class TestCharRuleBuilder(ELiDEAppTest):
+    def setUp(self):
+        super(TestCharRuleBuilder, self).setUp()
         with Engine(self.prefix) as eng:
             polygons.install(eng)
         app = self.app
         mgr = app.build()
-        win = window_with_widget(mgr)
+        self.win = window_with_widget(mgr)
         app.select_character(app.engine.character['triangle'])
         idle_until(lambda: app.character_name == 'triangle', 100,
                    'Never changed character')
         app.mainscreen.charmenu.charmenu.toggle_rules()
         idle_until(lambda: getattr(app.charrules, '_finalized', False), 100,
                    'Never finalized')
+
+    def test_char_rule_builder_display_avatar_trigger(self):
+        app = self.app
         tabitem = app.charrules._avatar_tab
         idle_until(lambda: tabitem.content, 100,
                    'avatar tab never got content')
