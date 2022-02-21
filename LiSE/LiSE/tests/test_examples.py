e22bf1a54d8b532f0a417221b04e382e71b29186
Zachary Spector <public@zacharyspector.com>
2022-02-21 18:37:24 -0500

    Add a test to catch that load error next time

--- LiSE/LiSE/tests/test_examples.py
+++ LiSE/LiSE/tests/test_examples.py
@@ -1,3 +1,4 @@
+from LiSE import Engine
 from LiSE.examples import college, kobold, polygons, sickle
 
 
@@ -17,6 +18,27 @@ def test_polygons(engy):
     polygons.install(engy)
     for i in range(10):
         engy.next_turn()
+
+
+def test_char_stat_startup(tempdir):
+    with Engine(tempdir) as eng:
+        tri = eng.new_character('triangle')
+        sq = eng.new_character('square')
+
+        sq.stat['min_sameness'] = 0.1
+        assert 'min_sameness' in sq.stat
+        sq.stat['max_sameness'] = 0.9
+        assert 'max_sameness' in sq.stat
+        tri.stat['min_sameness'] = 0.2
+        assert 'min_sameness' in tri.stat
+        tri.stat['max_sameness'] = 0.8
+        assert 'max_sameness' in tri.stat
+
+    with Engine(tempdir) as eng:
+        assert 'min_sameness' in eng.character['square'].stat
+        assert 'max_sameness' in eng.character['square'].stat
+        assert 'min_sameness' in eng.character['triangle'].stat
+        assert 'max_sameness' in eng.character['triangle'].stat
 
 
 def test_sickle(engy):
