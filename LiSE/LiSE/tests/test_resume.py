cc48a1d1da8a128c8c392dd337b2d37d52c5fe60
Zachary Spector <public@zacharyspector.com>
2022-03-26 22:36:42 -0400

    Add another assertion to test_resume

--- LiSE/LiSE/tests/test_resume.py
+++ LiSE/LiSE/tests/test_resume.py
@@ -7,7 +7,10 @@ def test_resume(tempdir):
     with Engine(tempdir) as eng:
         install(eng)
         eng.next_turn()
+        last_branch, last_turn, last_tick = eng._btt()
     with Engine(tempdir) as eng:
+        # tick gets bumped when we save the state of the randomizer
+        assert eng._btt() == (last_branch, last_turn, last_tick + 1)
         curturn = eng.turn
         eng.next_turn()
         assert eng.turn == curturn + 1
