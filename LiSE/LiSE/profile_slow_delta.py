ceb01587f5e68ae4cf00531f5aac9b6f08d81c96
Zachary Spector <public@zacharyspector.com>
2022-07-10 19:42:57 -0400

    Make profile_slow_delta.py install a sim and clean up after itself

--- LiSE/LiSE/profile_slow_delta.py
+++ LiSE/LiSE/profile_slow_delta.py
@@ -1,21 +1,25 @@
+import tempfile
 from time import monotonic
 
 from LiSE.handle import EngineHandle
+from LiSE.examples.polygons import install
 
-hand = EngineHandle(('/Users/zacha/PycharmProjects/LiSE/polys/', ), )
-print(-1)
-hand._real.cache_arrange_queue.join()
-ts = monotonic()
-print(0)
-hand.time_travel('trunk', 2)
-print(1, monotonic() - ts)
-ts = monotonic()
-hand.time_travel('truck', 1)
-print(2, monotonic() - ts)
-ts = monotonic()
-hand.time_travel('trunk', 0)
-print(3, monotonic() - ts)
-ts = monotonic()
-hand.time_travel('truck', 1)
-print(4, monotonic() - ts)
-hand.close()
+with tempfile.TemporaryDirectory() as dir:
+	hand = EngineHandle((dir, ), )
+	install(hand._real)
+	print(-1)
+	hand._real.cache_arrange_queue.join()
+	ts = monotonic()
+	print(0)
+	hand.time_travel('trunk', 2)
+	print(1, monotonic() - ts)
+	ts = monotonic()
+	hand.time_travel('truck', 1)
+	print(2, monotonic() - ts)
+	ts = monotonic()
+	hand.time_travel('trunk', 0)
+	print(3, monotonic() - ts)
+	ts = monotonic()
+	hand.time_travel('truck', 1)
+	print(4, monotonic() - ts)
+	hand.close()
