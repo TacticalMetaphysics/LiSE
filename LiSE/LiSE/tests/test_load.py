6138f97b90a3e190ba34f9f6d6a6a1ced45bb087
Zachary Spector <public@zacharyspector.com>
2022-07-17 08:21:57 -0400

    Remove unused imports

--- LiSE/LiSE/tests/test_load.py
+++ LiSE/LiSE/tests/test_load.py
@@ -12,11 +12,6 @@
 #
 # You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
-import os
-import shutil
-
-import pytest
-
 from LiSE.engine import Engine
 from LiSE.examples.kobold import inittest
 
