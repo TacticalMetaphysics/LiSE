f228caaa2ba6bcf448ccf50cd42c191ff487b7a7
Zachary Spector <public@zacharyspector.com>
2022-09-14 08:13:23 -0400

    Make the SQL overlap detection a little pickier
    When windows of time touch at the ends, but don't overlap, we shouldn't include them in the result.

--- LiSE/LiSE/query.py
+++ LiSE/LiSE/query.py
@@ -295,15 +295,15 @@ def _msfq_mid_turn(qry,
 	left_time_to_lt_right_time_from = and_(
 		left_sel.c.turn_to != None, right_sel.c.turn_from != None,
 		or_(
-			left_sel.c.turn_to < right_sel.c.turn_from,
+			left_sel.c.turn_to - 1 < right_sel.c.turn_from,
 			and_(left_sel.c.turn_to == right_sel.c.turn_from,
-					left_sel.c.tick_to < right_sel.c.tick_from)))
+					left_sel.c.tick_to - 1 < right_sel.c.tick_from)))
 	right_time_to_lt_left_time_from = and_(
 		right_sel.c.turn_to != None, left_sel.c.turn_from != None,
 		or_(
-			right_sel.c.turn_to < left_sel.c.turn_from,
+			right_sel.c.turn_to - 1 < left_sel.c.turn_from,
 			and_(right_sel.c.turn_to == right_sel.c.turn_from,
-					right_sel.c.tick_to < left_sel.c.tick_from)))
+					right_sel.c.tick_to - 1 < left_sel.c.tick_from)))
 	# then invert it
 	join = left_sel.alias().join(
 		right_sel.alias(),
@@ -328,10 +328,10 @@ def _msfq_end_turn(qry,
 	# overlap
 	left_time_to_lt_right_time_from = and_(
 		left_sel.c.turn_to != None, right_sel.c.turn_from != None,
-		left_sel.c.turn_to < right_sel.c.turn_from)
+		left_sel.c.turn_to - 1 < right_sel.c.turn_from)
 	right_time_to_lt_left_time_from = and_(
 		right_sel.c.turn_to != None, left_sel.c.turn_from != None,
-		right_sel.c.turn_to < left_sel.c.turn_from)
+		right_sel.c.turn_to - 1 < left_sel.c.turn_from)
 	join = left_sel.alias().join(
 		right_sel.alias(),
 		not_(
