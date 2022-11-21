7b707ce0989441a696bd6e3494df24e8b7b0b3ac
Zachary Spector <public@zacharyspector.com>
2022-11-20 23:11:03 -0500

    Fix keyframe format in _snap_keyframe_from_delta again

--- LiSE/LiSE/allegedb/__init__.py
+++ LiSE/LiSE/allegedb/__init__.py
@@ -1266,9 +1266,9 @@ class ORM:
 				assert copy_to_branch != now[0]
 				whens.append((copy_to_branch, now[1], now[2]))
 			for when in whens:
-				assert (graph, *when) not in kfl
-				nkfs.append((graph, *when, node_val_keyframe,
-								edge_val_keyframe, graph_val_keyframe))
+				nkfs.append(
+					(graph, *when, node_val_keyframe[graph],
+						edge_val_keyframe[graph], graph_val_keyframe[graph]))
 				kfl.append((graph, *when))
 				kfs.add(when)
 				branch, turn, tick = when
