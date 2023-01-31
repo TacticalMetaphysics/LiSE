1eb92c0cb2783225df34103fd32a0c29c2d65c39
Zachary Spector <public@zacharyspector.com>
2023-01-30 21:43:11 -0500

    Add test_update

--- ELiDE/ELiDE/tests/test_screen.py
+++ ELiDE/ELiDE/tests/test_screen.py
@@ -1,9 +1,9 @@
 from types import SimpleNamespace
 
 from kivy.base import EventLoop
-from kivy.config import ConfigParser
 from kivy.tests.common import UnitTestTouch
 
+from LiSE import Engine
 from LiSE.character import Facade
 from ELiDE.menu import DirPicker
 from ELiDE.screen import MainScreen
@@ -94,3 +94,38 @@ class ScreenTest(ELiDEAppTest):
 		motion.touch_up()
 		idle_until(lambda: int(turnfield.hint_text) == 3, 400,
 					"Time didn't advance fast enough")
+
+	def test_update(self):
+		with Engine(self.prefix) as eng:
+			phys = eng.new_character('physical')
+			here = phys.new_place((0, 0))
+			phys.add_place((1, 1))
+			phys.add_place(9)  # test that gridboard can handle this
+			this = here.new_thing(2)
+			@this.rule(always=True)
+			def go(me):
+				me["location"] = (1, 1)
+		app = self.app
+		app.starting_dir = self.prefix
+		app.build()
+		idle_until(lambda: app.engine is not None, 100, "Never got engine proxy")
+		assert app.engine.character['physical'].thing[2]['location'] == (0, 0)
+		graphboard = app.mainscreen.graphboards['physical']
+		gridboard = app.mainscreen.gridboards['physical']
+		idle_until(lambda: graphboard.size != [100, 100], 100, "Never resized graphboard")
+		idle_until(lambda: gridboard.size != [100, 100], 100, "Never resized gridboard")
+		idle_until(lambda: (0, 0) in graphboard.spot, 100, "Never made spot for location 0")
+		locspot0 = graphboard.spot[0, 0]
+		gridspot0 = gridboard.spot[0, 0]
+		locspot1 = graphboard.spot[1, 1]
+		gridspot1 = gridboard.spot[1, 1]
+		graphpawn = graphboard.pawn[2]
+		gridpawn = gridboard.pawn[2]
+		idle_until(lambda: graphpawn.x == locspot0.right, 100, "Never positioned pawn to 0's right")
+		idle_until(lambda: graphpawn.y == locspot0.top, 100, "Never positioned pawn to 0's top")
+		idle_until(lambda: gridpawn.pos == gridspot0.pos, 100, "Never positioned pawn to grid 0, 0")
+		app.mainscreen.next_turn()
+		assert app.engine.character['physical'].thing[2]['location'] == (1, 1)
+		idle_until(lambda: graphpawn.x == locspot1.right, 100, "Never positioned pawn to 1's right")
+		idle_until(lambda: graphpawn.y == locspot1.top, 100, "Never positioned pawn to 1's top")
+		idle_until(lambda: gridpawn.pos == gridspot1.pos, 100, "Never positioned pawn to grid 1, 1")
