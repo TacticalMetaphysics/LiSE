cf54eb6b54d0a340427b558d11bf196116aec700
Zachary Spector <public@zacharyspector.com>
2023-03-19 13:40:49 -0400

    Fix `test_resume`
    Now that we save the state of the randomizer in `flush` rather than `commit`

--- LiSE/LiSE/tests/test_resume.py
+++ LiSE/LiSE/tests/test_resume.py
@@ -9,8 +9,7 @@ def test_resume(tempdir):
 		eng.next_turn()
 		last_branch, last_turn, last_tick = eng._btt()
 	with Engine(tempdir) as eng:
-		# tick gets bumped when we save the state of the randomizer
-		assert eng._btt() == (last_branch, last_turn, last_tick + 1)
+		assert eng._btt() == (last_branch, last_turn, last_tick )
 		curturn = eng.turn
 		eng.next_turn()
 		assert eng.turn == curturn + 1
