ff275cf9e6c2a4383896749654fcff83b93c7b65
Zachary Spector <public@zacharyspector.com>
2023-03-27 06:56:04 -0400

    Decruft

--- LiSE/readprof.py
+++
@@ -1,9 +0,0 @@
-import pstats
-import sys
-
-stats = pstats.Stats()
-if len(sys.argv) > 1:
-	stats.load_stats(sys.argv[-1])
-else:
-	stats.load_stats('dump.prof')
-stats.sort_stats('cumtime').reverse_order().print_stats()
