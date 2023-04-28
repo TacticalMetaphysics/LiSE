9c26ba587a50b40369fc98eb09eb00464cc976a8
Zachary Spector <public@zacharyspector.com>
2023-04-27 22:12:51 -0400

    Fix twine calls in release.sh

--- release.sh
+++ release.sh
@@ -3,7 +3,7 @@ set -euxo
 python -m tox -p4
 rm -rf LiSE/dist
 python -m build LiSE/
-python -m twine --repository LiSE LiSE/dist/*
+python -m twine upload --repository LiSE LiSE/dist/*
 rm -rf ELiDE/dist
 python -m build ELiDE/
-python -m twine --repository ELiDE ELiDE/dist/*
+python -m twine upload --repository ELiDE ELiDE/dist/*
