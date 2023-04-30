f252e1861cd5cb89f6a2fe73b826e3b306569e28
Zachary Spector <public@zacharyspector.com>
2023-04-30 09:43:28 -0400

    Improve release.sh

--- release.sh
+++ release.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 set -euxo
-python -m tox -p4
+python -m tox -p auto
 rm -rf LiSE/dist
 python -m build LiSE/
-python -m twine upload --repository LiSE LiSE/dist/*
 rm -rf ELiDE/dist
 python -m build ELiDE/
+python -m twine check LiSE/dist/* ELiDE/dist/*
+python -m twine upload --repository LiSE LiSE/dist/*
 python -m twine upload --repository ELiDE ELiDE/dist/*
