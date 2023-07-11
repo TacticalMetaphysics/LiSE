6184e2900c0ff5329ec9a6f3b65bae83d7af305b
Zachary Spector <public@zacharyspector.com>
2023-07-11 00:02:21 -0400

    Turn `remake_college24.py` into its own script

---
+++ LiSE/LiSE/tests/remake_college24.py
@@ -0,0 +1,17 @@
+import os
+import tempfile
+import shutil
+
+from ..engine import Engine
+from ..examples.college import install
+
+outpath = os.path.join(os.path.abspath(os.path.dirname(__file__)),
+						'college24_premade.tar.xz')
+if os.path.exists(outpath):
+	os.remove(outpath)
+with tempfile.TemporaryDirectory() as directory:
+	with Engine(directory) as eng:
+		install(eng)
+		for i in range(24):
+			eng.next_turn()
+	shutil.make_archive(outpath[:-7], 'xztar', directory, '.')
