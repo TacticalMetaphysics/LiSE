e3572c547e51ca98daed1bff131595dfa09eac44
Zachary Spector <public@zacharyspector.com>
2024-06-22 14:09:25 +1200

    Remove things more aggressively in release.sh

--- release.sh
+++ release.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 set -euxo
 python -m tox -p auto
-rm -rf LiSE/dist
+rm -rf LiSE/build LiSE/dist
 python -m build LiSE/
-rm -rf ELiDE/dist
+rm -rf ELiDE/build ELiDE/dist
 python -m build ELiDE/
 python -m twine check LiSE/dist/* ELiDE/dist/*
 python -m twine upload --repository LiSE LiSE/dist/*
 python -m twine upload --repository ELiDE ELiDE/dist/*
+rm -rf LiSE/build LiSE/dist ELiDE/build ELiDE/dist
