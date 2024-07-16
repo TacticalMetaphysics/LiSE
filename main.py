914d1f457b212cb024f5e572a5bc031f497af5f3
Zachary Spector <public@zacharyspector.com>
2024-07-17 09:18:31 +1200

    ruff

--- main.py
+++ main.py
@@ -3,14 +3,14 @@ import sys
 import os
 
 wd = os.getcwd()
-sys.path.extend([wd + '/LiSE', wd + '/ELiDE'])
+sys.path.extend([wd + "/LiSE", wd + "/ELiDE"])
 
 
 def get_application_config(*args):
-	return wd + '/ELiDE.ini'
+	return wd + "/ELiDE.ini"
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
 	freeze_support()
 
 	from ELiDE.app import ELiDEApp
