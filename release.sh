d6c0bab104c4a82a9155d5cd78c623cf3ecf0579
Zachary Spector <public@zacharyspector.com>
2024-08-11 01:01:32 +1200

    Release v0.15.5
    Updating `release.sh` in the process. Now doing the tox tests in serial because they kept clobbering each other's work on the wheel files.

--- release.sh
+++ release.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 set -euxo
-python -m tox -p auto
+python -m tox
 rm -rf LiSE/build LiSE/dist
 python -m build LiSE/
 rm -rf ELiDE/build ELiDE/dist
 python -m build ELiDE/
 python -m twine check LiSE/dist/* ELiDE/dist/*
-python -m twine upload --repository LiSE LiSE/dist/*
-python -m twine upload --repository ELiDE ELiDE/dist/*
+python -m twine upload LiSE/dist/*
+python -m twine upload ELiDE/dist/*
 rm -rf LiSE/build LiSE/dist ELiDE/build ELiDE/dist
