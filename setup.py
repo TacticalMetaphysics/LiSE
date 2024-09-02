17b1f26fbe0cea5e018f6b880948a322de8de748
Zachary Spector <public@zacharyspector.com>
2024-09-03 05:41:02 +1200

    Add dependencies to setup.py
    "Parsing" TOML by hand. Whatever...

--- setup.py
+++ setup.py
@@ -12,6 +12,21 @@ with open(os.path.join(here, "LiSE", "pyproject.toml"), "rt") as inf:
 	else:
 		raise ValueError("Couldn't get version")
 
+deps = []
+for subpkg in ["LiSE", "ELiDE"]:
+	with open(os.path.join(here, subpkg, "pyproject.toml"), "rt") as inf:
+		for line in inf:
+			if line.startswith("dependencies"):
+				break
+		else:
+			raise ValueError("Couldn't get %s dependencies" % subpkg)
+		for line in inf:
+			if line == "]":
+				break
+			deps.append(line)
+		else:
+			raise ValueError("%s dependencies never ended" % subpkg)
+
 setup(
 	name="ELiDE_bundle",
 	version=version,
@@ -21,4 +36,5 @@ setup(
 		"LiSE": os.path.join(here, "LiSE", "LiSE"),
 		"ELiDE": os.path.join(here, "ELiDE", "ELiDE"),
 	},
+	install_requires=deps,
 )
