7a2a59167d96c28ce454a02ef1a8c74fe2c29c53
Zachary Spector <public@zacharyspector.com>
2024-09-03 05:25:09 +1200

    Add a setup.py for people installing via `git+https://`

---
+++ setup.py
@@ -0,0 +1,26 @@
+import os
+
+from setuptools import setup
+
+with open(
+	os.path.join(os.path.dirname(__file__), "LiSE", "pyproject.toml"), "rt"
+) as inf:
+	for line in inf:
+		if line.startswith("version"):
+			_, version, _ = line.split('"')
+			break
+	else:
+		raise ValueError("Couldn't get version")
+
+setup(
+	name="ELiDE_bundle",
+	version=version,
+	packages=["LiSE", "ELiDE", "LiSE.allegedb"],
+	package_dir={
+		"LiSE": os.path.join(os.path.dirname(__file__), "LiSE", "LiSE"),
+		"ELiDE": os.path.join(os.path.dirname(__file__), "ELiDE", "ELiDE"),
+		"allegedb": os.path.join(
+			os.path.dirname(__file__), "LiSE", "LiSE", "allegedb"
+		),
+	},
+)
