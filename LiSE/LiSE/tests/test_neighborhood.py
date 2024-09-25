26c9fbb4bed891ed152a7ce08133843cc4c3c89f
Zachary Spector <public@zacharyspector.com>
2024-09-26 07:23:11 +1200

    Fix `test_neighborhood.test_sim_start`

--- LiSE/LiSE/tests/test_neighborhood.py
+++ LiSE/LiSE/tests/test_neighborhood.py
@@ -12,12 +12,18 @@ def test_sim_start(three3):
 	@three3.place.rule(neighborhood=1, always=True)
 	def did_it_run(place):
 		if "it_ran" in place:
-			place["it_ran"] += 1
+			place.character.stat["it_ran"] = (
+				place.character.stat.get("it_ran", 0) + 1
+			)
 		else:
-			place["it_ran"] = 1
+			place["it_ran"] = True
 
 	eng = three3.engine
 	eng.next_turn()
-	assert three3.place[1, 1]["it_ran"] == 1
+	assert three3.place[1, 1]["it_ran"]
+	assert "it_ran" not in three3.stat
 	eng.next_turn()
-	assert three3.place[1, 1]["it_ran"] == 1
+	assert three3.place[1, 1]["it_ran"]
+	assert three3.stat["it_ran"] == 9
+	eng.next_turn()
+	assert three3.stat["it_ran"] == 9  # still!
