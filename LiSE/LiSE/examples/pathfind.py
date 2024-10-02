e58a3681edff935d13387384cefc819ea5ac09ec
Zachary Spector <public@zacharyspector.com>
2024-10-02 13:05:09 +1300

    Add a test sim for process pools

---
+++ LiSE/LiSE/examples/pathfind.py
@@ -0,0 +1,51 @@
+import random
+
+import networkx as nx
+
+
+def install(eng, seed=None):
+	if seed is not None:
+		random.seed(seed)
+	grid: nx.Graph = nx.grid_2d_graph(100, 100)
+
+	for node in list(grid):
+		if random.choice((True, False)):
+			grid.remove_node(node)
+		elif random.random() < 0.1:
+			grid.add_node(f"{node}_inhabitant", location=node)
+
+	phys = eng.new_character("physical", grid)
+
+	@eng.function
+	def find_path_somewhere(node):
+		x, y = node.name
+		destx = 100 - x
+		desty = 100 - y
+		while (destx, desty) not in node.character.place:
+			if destx < 99:
+				destx += 1
+			elif desty < 99:
+				destx = 0
+				desty += 1
+			else:
+				destx = desty = 0
+		return node.shortest_path(node.character.place[destx, desty])
+
+	@phys.rule
+	def go_places(char):
+		futs = []
+		with char.engine.pool as pool:
+			for thing in char.thing:
+				fut = pool.submit(
+					char.engine.function.find_path_somewhere, thing
+				)
+				fut.thing = thing
+				futs.append(fut)
+		for fut in futs:
+			result = fut.result()
+			thing = fut.thing
+			thing.follow_path(result)
+
+	@go_places.trigger
+	def turn_one_only(char):
+		return char.engine.turn == 1
