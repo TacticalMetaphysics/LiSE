efe9f66d22b28e4ec65e195c8397ca2047931c06
Zachary Spector <public@zacharyspector.com>
2024-10-04 15:51:10 +1300

    Use with-blocks in test_keyframe_load_init
    So that they clean up after themselves

--- LiSE/LiSE/tests/test_load.py
+++ LiSE/LiSE/tests/test_load.py
@@ -18,16 +18,13 @@ from LiSE.examples.kobold import inittest
 
 def test_keyframe_load_init(tempdir):
 	"""Can load a keyframe at start of branch, including locations"""
-	eng = Engine(tempdir)
-	inittest(eng)
-	eng.branch = "new"
-	eng.snap_keyframe()
-	eng.close()
-	eng = Engine(tempdir)
-	assert "kobold" in eng.character["physical"].thing
-	assert (0, 0) in eng.character["physical"].place
-	assert (0, 1) in eng.character["physical"].portal[0, 0]
-	eng.close()
+	with Engine(tempdir) as eng:
+		inittest(eng)
+		eng.branch = "new"
+	with Engine(tempdir) as eng:
+		assert "kobold" in eng.character["physical"].thing
+		assert (0, 0) in eng.character["physical"].place
+		assert (0, 1) in eng.character["physical"].portal[0, 0]
 
 
 def test_multi_keyframe(tempdir):
