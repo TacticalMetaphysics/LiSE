41f31277d6ffc76b18d2352c901fbeb389372bbe
Zachary Spector <public@zacharyspector.com>
2024-10-07 21:44:34 +1300

    Be less clever about `universal_set`

--- LiSE/LiSE/query.py
+++ LiSE/LiSE/query.py
@@ -1677,31 +1677,6 @@ class ParquetDBHolder:
 				else:
 					yield d["thing"], d["turn"], d["tick"], d["location"]
 
-	def universal_set(
-		self, key: bytes, branch: str, turn: int, tick: int, val: bytes
-	):
-		id_ = self.filter_get_id(
-			"universals",
-			[
-				pc.field("key") == key,
-				pc.field("branch") == branch,
-				pc.field("turn") == turn,
-				pc.field("tick") == tick,
-			],
-		)
-		item = {
-			"key": key,
-			"branch": branch,
-			"turn": turn,
-			"tick": tick,
-			"value": val,
-		}
-		if id_ is None:
-			self.insert1("universals", item)
-		else:
-			item["id"] = id_
-			self._db.update(item, "universals")
-
 	@staticmethod
 	def echo(it):
 		return it
@@ -2914,7 +2889,17 @@ class ParquetQueryEngine(AbstractLiSEQueryEngine):
 		self, key: Key, branch: str, turn: int, tick: int, val: Any
 	):
 		pack = self.pack
-		self.call("universal_set", pack(key), branch, turn, tick, pack(val))
+		self.call(
+			"insert",
+			"universal",
+			{
+				"key": pack(key),
+				"branch": branch,
+				"turn": turn,
+				"tick": tick,
+				"value": pack(val),
+			},
+		)
 
 	def universal_del(self, key: Key, branch: str, turn: int, tick: int):
 		self.universal_set(key, branch, turn, tick, b"\xc0")
