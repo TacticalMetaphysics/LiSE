0097e2040321675ded796858320a7c65295937c6
Zachary Spector <public@zacharyspector.com>
2024-10-07 22:05:08 +1300

    Implement `handled_node_rule`

--- LiSE/LiSE/query.py
+++ LiSE/LiSE/query.py
@@ -3263,7 +3263,20 @@ class ParquetQueryEngine(AbstractLiSEQueryEngine):
 		turn: int,
 		tick: int,
 	):
-		pass
+		pack = self.pack
+		self.call(
+			"insert1",
+			"node_rules_handled",
+			dict(
+				character=pack(character),
+				node=pack(node),
+				rulebook=pack(rulebook),
+				rule=rule,
+				branch=branch,
+				turn=turn,
+				tick=tick,
+			),
+		)
 
 	def handled_portal_rule(
 		self,
