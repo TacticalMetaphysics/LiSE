fcdde8ab8b44349aa8a89f0264cdd60ee70dafbb
Zachary Spector <public@zacharyspector.com>
2024-10-22 11:40:12 +1300

    Stop using a batch in `load_at`
    It's redundant with what the `load` method already does

--- LiSE/LiSE/allegedb/core.py
+++ LiSE/LiSE/allegedb/core.py
@@ -2134,19 +2134,11 @@ class ORM:
 					future_tick,
 				),
 			)
-		if self._no_kc:
-			self._nodes_cache.load(noderows)
-			self._edges_cache.load(edgerows)
-			self._graph_val_cache.load(graphvalrows)
-			self._node_val_cache.load(nodevalrows)
-			self._edge_val_cache.load(edgevalrows)
-		else:
-			with self.batch():
-				self._nodes_cache.load(noderows)
-				self._edges_cache.load(edgerows)
-				self._graph_val_cache.load(graphvalrows)
-				self._node_val_cache.load(nodevalrows)
-				self._edge_val_cache.load(edgevalrows)
+		self._nodes_cache.load(noderows)
+		self._edges_cache.load(edgerows)
+		self._graph_val_cache.load(graphvalrows)
+		self._node_val_cache.load(nodevalrows)
+		self._edge_val_cache.load(edgevalrows)
 
 		return (
 			latest_past_keyframe,
