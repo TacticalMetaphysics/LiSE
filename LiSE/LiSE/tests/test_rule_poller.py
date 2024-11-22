c44b597a11c772c3eca2feaf9ecc297b387f5dab
Zachary Spector <public@zacharyspector.com>
2024-11-23 11:00:48 +1300

    Implement `test_unit_rule_poll`

--- LiSE/LiSE/tests/test_rule_poller.py
+++ LiSE/LiSE/tests/test_rule_poller.py
@@ -14,3 +14,27 @@ def test_character_rule_poll(engy):
 
 	assert "run" in phys.stat
 	assert "run" not in notphys.stat
+
+
+def test_unit_rule_poll(engy):
+	phys = engy.new_character("physical")
+	notphys = engy.new_character("ethereal")
+
+	unit = phys.new_place("unit")
+	notunit1 = notphys.new_place("notunit")
+	notunit2 = phys.new_place("notunit")
+	notphys.add_unit(unit)
+
+	@notphys.unit.rule(always=True)
+	def rule1(unit):
+		unit["run"] = True
+
+	@phys.unit.rule
+	def rule2(unit):
+		unit["run"] = True
+
+	engy.next_turn()
+
+	assert unit["run"]
+	assert "run" not in notunit1
+	assert "run" not in notunit2
