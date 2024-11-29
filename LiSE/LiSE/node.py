145e615ab4b7dcb7f206ba5f8f07613d2bcdbe9a
Zachary Spector <public@zacharyspector.com>
2024-11-29 21:58:19 +1300

    Fix user iteration

--- LiSE/LiSE/node.py
+++ LiSE/LiSE/node.py
@@ -70,7 +70,7 @@ class UserMapping(Mapping):
 					if engine._unitness_cache.user_cache.retrieve(
 						charn, nn, user, b, r, t
 					):
-						yield user
+						yield user[0]
 				except KeyError:
 					continue
 
